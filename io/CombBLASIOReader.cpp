59160a22be06bed19ca83b4d98c444193cab22af
Isuru Ranawaka <irjanith@gmail.com>
2023-05-03 10:47:56 -0400

    remove header

--- io/CombBLASIOReader.cpp
+++ io/CombBLASIOReader.cpp
@@ -3,16 +3,17 @@
  */
 #include "CombBLAS/CombBLAS.h"
 
-using namespace distblas::io;
 using namespace combblas;
+
+namespace distblas::io {
 
 typedef SpParMat<int64_t, int, SpDCCols<int32_t, int>> PSpMat_s32p64_Int;
 
-template <typename T>
-ParallelIO<T>::ParallelIO() {}
+template <typename T> ParallelIO<T>::ParallelIO() {}
 
 template <typename T>
-void ParallelIO<T>::parallel_read_MM(string file_path, vector<Tuple<T>> &coords) {
+void ParallelIO<T>::parallel_read_MM(string file_path,
+                                     vector<Tuple<T>> &coords) {
   MPI_Comm WORLD;
   MPI_Comm_dup(MPI_COMM_WORLD, &WORLD);
 
@@ -23,7 +24,8 @@ void ParallelIO<T>::parallel_read_MM(string file_path, vector<Tuple<T>> &coords)
   shared_ptr<CommGrid> simpleGrid;
   simpleGrid.reset(new CommGrid(WORLD, num_procs, 1));
 
-  unique_ptr<PSpMat_s32p64_Int> G = unique_ptr<PSpMat_s32p64_Int>(new PSpMat_s32p64_Int(simpleGrid));
+  unique_ptr<PSpMat_s32p64_Int> G =
+      unique_ptr<PSpMat_s32p64_Int>(new PSpMat_s32p64_Int(simpleGrid));
 
   uint64_t nnz;
 
@@ -34,18 +36,16 @@ void ParallelIO<T>::parallel_read_MM(string file_path, vector<Tuple<T>> &coords)
     cout << "File reader read " << nnz << " nonzeros." << endl;
   }
   SpTuples<int64_t, T> tups(G.get()->seq());
-  tuple<int64_t, int64_t, T>* values = tups.tuples;
+  tuple<int64_t, int64_t, T> *values = tups.tuples;
 
-   coords.resize(tups.getnnz());
+  coords.resize(tups.getnnz());
 
-  for(int i = 0; i < tups.getnnz(); i++) {
+  for (int i = 0; i < tups.getnnz(); i++) {
     coords[i].row = get<0>(values[i]);
     coords[i].col = get<1>(values[i]);
     coords[i].value = get<2>(values[i]);
   }
-
 }
 
-
-template <typename T>
-ParallelIO<T>::~ParallelIO() {}
+template <typename T> ParallelIO<T>::~ParallelIO() {}
+}
