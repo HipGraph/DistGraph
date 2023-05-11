52a035a79ce2d9bea183799500ce49e52467effe
Isuru Ranawaka <irjanith@gmail.com>
2023-05-11 08:12:57 -0400

    fix compilation issues

--- cpp/partition/partitioner.hpp
+++ cpp/partition/partitioner.hpp
@@ -12,7 +12,7 @@ namespace distblas::partition  {
     ~Partitioner();
 
     template <typename T>
-    void redistribute_data(const shared_ptr<SpMat<T>> &spmat_ptr) {
+    void redistribute_data(const shared_ptr<SpMat> &spmat_ptr) {
       int num_procs, proc_rank;
       MPI_Comm_size(this->comm, &num_procs);
       MPI_Comm_rank(this->comm, &proc_rank);
