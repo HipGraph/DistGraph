02bbc9f23c13f75af826c440bdb5c8f25d94da9c
Isuru Ranawaka <irjanith@gmail.com>
2023-05-29 14:55:31 -0400

    Adding common.cpp

--- cpp/core/common.cpp
+++ cpp/core/common.cpp
@@ -5,7 +5,7 @@ using namespace std;
 
 MPI_Datatype SPTUPLE;
 
-int divide_and_round_up(int num, int denom){
+int distblas::core::divide_and_round_up(int num, int denom){
   if (num % denom > 0) {
     return num / denom + 1;
   }
@@ -15,7 +15,7 @@ int divide_and_round_up(int num, int denom){
 }
 
 
-void prefix_sum(vector<int> &values, vector<int> &offsets) {
+void distblas::core::prefix_sum(vector<int> &values, vector<int> &offsets) {
   int sum = 0;
   for(int i = 0; i < values.size(); i++) {
     offsets.push_back(sum);
--- cpp/core/common.h
+++ cpp/core/common.h
@@ -1,62 +1,57 @@
 #ifndef COMMON_HEADER
 #define COMMON_HEADER
 
-#include <cstdint> // int64_t
-#include <vector>
-#include <mpi.h>
 #include <cstddef>
+#include <cstdint> // int64_t
+#include <mpi.h>
+#include <vector>
 
 using namespace std;
 
 namespace distblas::core {
 
-template <typename T>
-struct Tuple {
+int divide_and_round_up(int num, int denom);
+
+void prefix_sum(vector<int> &values, vector<int> &offsets);
+
+template <typename T> struct Tuple {
   int64_t row;
   int64_t col;
   T value;
 };
 
-template <typename T>
-struct CSR {
+template <typename T> struct CSR {
   int64_t row;
   int64_t col;
   T value;
 };
 
-
-
-template <typename T>
-bool column_major(Tuple<T> &a, Tuple<T> &b) {
-  if(a.col == b.col) {
+template <typename T> bool column_major(Tuple<T> &a, Tuple<T> &b) {
+  if (a.col == b.col) {
     return a.row < b.row;
-  }
-  else {
-    return a.col< b.col;
+  } else {
+    return a.col < b.col;
   }
 }
 
-template <typename T>
-bool row_major(Tuple<T> &a, Tuple<T> &b) {
-  if(a.row == b.row) {
+template <typename T> bool row_major(Tuple<T> &a, Tuple<T> &b) {
+  if (a.row == b.row) {
     return a.col < b.col;
-  }
-  else {
+  } else {
     return a.row < b.row;
   }
 }
 
 extern MPI_Datatype SPTUPLE;
 
-template <typename T>
-void initialize_mpi_datatypes() {
+template <typename T> void initialize_mpi_datatypes() {
   const int nitems = 3;
   int blocklengths[3] = {1, 1, 1};
   MPI_Datatype types[3];
   if (std::is_same<T, int>::value) {
     types[3] = {MPI_UINT64_T, MPI_UINT64_T, MPI_INT};
   } else {
-    //TODO:Need to support all datatypes
+    // TODO:Need to support all datatypes
     types[3] = {MPI_UINT64_T, MPI_UINT64_T, MPI_DOUBLE};
   }
 
@@ -68,10 +63,6 @@ void initialize_mpi_datatypes() {
   MPI_Type_commit(&SPTUPLE);
 }
 
-int divide_and_round_up(int num, int denom);
-
-void prefix_sum(vector<int> &values, vector<int> &offsets);
-
-} // namespace distblas::core
+}; // namespace distblas::core
 
 #endif
