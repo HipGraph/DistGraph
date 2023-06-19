ba185db4b5145e5886fcbbc85cfc9ff6deeef783
Isuru Ranawaka <irjanith@gmail.com>
2023-06-19 14:53:58 -0400

    cpp/partition/partitioner.hpp

--- cpp/partition/partitioner.hpp
+++ cpp/partition/partitioner.hpp
@@ -109,7 +109,7 @@ namespace distblas::partition  {
       cout<<" rank "<< my_rank << " MPI_Alltoallv completed "<<endl;
 
       // TODO: Parallelize the sort routine?
-      std::sort((sp_mat->coords).begin(), (sp_mat->coords).end(), column_major<T>);
+      std::sort(std::execution::par,(sp_mat->coords).begin(), (sp_mat->coords).end(), column_major<T>);
 //      __gnu_parallel::sort((sp_mat->coords).begin(), (sp_mat->coords).end(), column_major<T>);
       cout<<" rank "<< my_rank << " delete sorting completeed "<<endl;
       delete[] sendbuf;
