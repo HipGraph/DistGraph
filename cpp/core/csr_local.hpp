d2b7a8dc1adb3fd3f7529306772d32857fb0fcf4
Isuru Ranawaka <irjanith@gmail.com>
2023-06-21 13:59:46 -0400

    debugging

--- cpp/core/csr_local.hpp
+++ cpp/core/csr_local.hpp
@@ -82,7 +82,7 @@ public:
       fout<<" rank "<<rank<<" "<< rArray[i] << " "<< cArray[i]<<" " << vArray[i] <<endl;
     }
 
-    mkl_set_num_threads(2);
+    mkl_set_num_threads(1);
 
     sparse_operation_t op;
     if (transpose) {
