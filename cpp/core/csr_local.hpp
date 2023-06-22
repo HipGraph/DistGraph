3c9ea92e69271a99ceaefa2455dfec049552dded
Isuru Ranawaka <irjanith@gmail.com>
2023-06-21 20:05:42 -0400

    debugging

--- cpp/core/csr_local.hpp
+++ cpp/core/csr_local.hpp
@@ -51,9 +51,9 @@ public:
 
 
     // This setup is really clunky, but I don't have time to fix it.
-    vector<MKL_INT> rArray(5000, 0);
-    vector<MKL_INT> cArray(5000, 0);
-    vector<double> vArray(5000, 0.0);
+    vector<MKL_INT> rArray(20000, 0);
+    vector<MKL_INT> cArray(20000, 0);
+    vector<double> vArray(20000, 0.0);
 //
 //    // Put a dummy value in if the number of coordinates is 0, so that
 //    // everything doesn't blow up
@@ -66,7 +66,7 @@ public:
 
 
 //#pragma omp parallel for
-    for (int i = 0; i < 5000; i++) {
+    for (int i = 0; i < 20000; i++) {
 //      rArray[i] = coords[i].row;
 //      cArray[i] = coords[i].col;
       rArray[i] = i;
@@ -103,7 +103,7 @@ public:
     cout<<" rank "<< rank <<" number of coords "<< num_coords << " attempting to create coo "<<endl;
 
     sparse_status_t  status_coo = mkl_sparse_d_create_coo(&tempCOO, SPARSE_INDEX_BASE_ZERO, 30000, 30000,
-                            max(5000, 1), rArray.data(), cArray.data(),
+                            max(20000, 1), rArray.data(), cArray.data(),
                             vArray.data());
 
     if (status_coo != SPARSE_STATUS_SUCCESS) {
