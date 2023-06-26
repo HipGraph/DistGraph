6d581169ef8a910e6f281f727954061221437a33
Isuru Ranawaka <irjanith@gmail.com>
2023-06-26 08:33:45 -0400

    adding EIGEN

--- cpp/core/dense_mat.hpp
+++ cpp/core/dense_mat.hpp
@@ -9,19 +9,17 @@ using Eigen::MatrixXd;
 namespace distblas::core {
 
 class DenseMat : DistributedMat {
-
+public:
   DenseMat() {
-    MatrixXd m(2,2);
-    m(0,0) = 3;
-    m(1,0) = 2.5;
-    m(0,1) = -1;
-    m(1,1) = m(1,0) + m(0,1);
+    MatrixXd m(2, 2);
+    m(0, 0) = 3;
+    m(1, 0) = 2.5;
+    m(0, 1) = -1;
+    m(1, 1) = m(1, 0) + m(0, 1);
     std::cout << m << std::endl;
   }
 
-  ~DenseMat() {
-
-  }
+  ~DenseMat() {}
 };
 
 } // namespace distblas::core
