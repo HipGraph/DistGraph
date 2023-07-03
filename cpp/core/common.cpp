4706e7bc59114db9157f51a35cc586c2a7e268cb
Isuru Ranawaka <irjanith@gmail.com>
2023-07-03 13:26:53 -0400

    matched count

--- cpp/core/common.cpp
+++ cpp/core/common.cpp
@@ -4,6 +4,7 @@
 using namespace std;
 
 MPI_Datatype distblas::core::SPTUPLE;
+MPI_Datatype distblas::core::DENSETUPLE;
 
 int distblas::core::divide_and_round_up(int num, int denom){
   if (num % denom > 0) {
