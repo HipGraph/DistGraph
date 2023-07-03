bba068833e2e8bc422a8fd3d014507bd3a5bd852
Isuru Ranawaka <irjanith@gmail.com>
2023-07-03 14:40:15 -0400

    matched count

--- cpp/net/data_comm.hpp
+++ cpp/net/data_comm.hpp
@@ -5,7 +5,7 @@
 #include <mpi.h>
 #include <vector>
 
-using namespace distblas::core
+using namespace distblas::core;
 
     namespace distblas::net {
 
@@ -13,35 +13,35 @@ using namespace distblas::core
    * This class represents the data transfer related operations across processes
    * based on internal data connectivity patterns.
    */
-  template <typename SPT, DENT>
+  template <typename SPT, typename  DENT>
   class DataComm {
 
   private:
-    SpMat<SPT> *sp_local;
-    SpMat<SPT> *sp_local_trans;
-    DenseMat *dense_local;
+    disblas::core::SpMat<SPT> *sp_local;
+    disblas::core::SpMat<SPT> *sp_local_trans;
+    disblas::core::DenseMat *dense_local;
 
   public:
-    DataComm(SpMat<SPT> *sp_local, SpMat<SPT> *sp_local_trans,
+    DataComm(disblas::core::SpMat<SPT> *sp_local, disblas::core::SpMat<SPT> *sp_local_trans,
              DenseMat *dense_local) {
       this->sp_local = sp_local;
       this->sp_local_trans = sp_local_trans;
       this->dense_local = dense_local;
 
-      void invoke(int batch_id, bool fetch_all) {
+    }
+    void invoke(int batch_id, bool fetch_all) {
 
-        int no_of_nodes_per_proc_list =
-            (this->sp_local->proc_col_width / this->sp_local->block_col_width);
-        int no_of_nodes_per_proc_list_trans =
-            (this->sp_local_trans->proc_row_width /
-             this->sp_local_trans->block_row_width);
+      int no_of_nodes_per_proc_list =
+          (this->sp_local->proc_col_width / this->sp_local->block_col_width);
+      int no_of_nodes_per_proc_list_trans =
+          (this->sp_local_trans->proc_row_width /
+           this->sp_local_trans->block_row_width);
 
-        int no_of_lists =
-            (this->sp_local->proc_col_width / this->sp_local->block_col_width);
+      int no_of_lists =
+          (this->sp_local->proc_col_width / this->sp_local->block_col_width);
 
-        int no_of_lists_trans = (this->sp_local_trans->proc_row_width /
-                                 this->sp_local_trans->block_row_width);
-      }
+      int no_of_lists_trans = (this->sp_local_trans->proc_row_width /
+                               this->sp_local_trans->block_row_width);
     }
   };
 }
