8fdea1083a3c6597a2f8f1712ff60bf9d9e4875a
Isuru Ranawaka <irjanith@gmail.com>
2023-07-04 11:30:48 -0400

    matched count

--- tests/data_structure_tests.cpp
+++ tests/data_structure_tests.cpp
@@ -110,10 +110,10 @@ int main(int argc, char **argv) {
 //    dense_mat.get()->print_matrix();
     cout<<" rank "<< rank << " creation of dense matrices completed  "<<endl;
 
-  auto communicator = unique_ptr<DataComm<int,double>>(new DataComm<int,double>(shared_sparseMat.get(),
-                                                                                    shared_sparseMat_Trans.get(),
-                                                                                    dense_mat.get(),
-                                                                                    grid.get()));
+//  auto communicator = unique_ptr<DataComm<int,double>>(new DataComm<int,double>(shared_sparseMat.get(),
+//                                                                                    shared_sparseMat_Trans.get(),
+//                                                                                    dense_mat.get(),
+//                                                                                    grid.get()));
 //  communicator.get()->invoke(0,true);
 
 
