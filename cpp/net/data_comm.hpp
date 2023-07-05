d18b7b670f3ce2f418477b03364000abb6fd7327
Isuru Ranawaka <irjanith@gmail.com>
2023-07-05 15:30:20 -0400

    matched count

--- cpp/net/data_comm.hpp
+++ cpp/net/data_comm.hpp
@@ -249,10 +249,10 @@ public:
       cout << " rank " << grid->global_rank << " receving  from src " << i
            << " count " << receivecounts[i] << endl;
       for (int j = 0; j < sending_vec.size(); j++) {
-        int index = sdispls[i] + j;
-        DataTuple<DENT> tuple(dense_local->cols);
-        tuple.col = sending_vec[j];
-        sendbuf[index] = tuple;
+//        int index = sdispls[i] + j;
+//        DataTuple<DENT> tuple(dense_local->cols);
+//        tuple.col = sending_vec[j];
+        sendbuf[index].col = sending_vec[j];
       }
 
       for (int j = 0; j < receiving_vec.size(); j++) {
