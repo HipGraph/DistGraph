841c897bdb4804bb7d87f952b74b72b706e7ac7b
Isuru Ranawaka <irjanith@gmail.com>
2023-07-05 18:21:31 -0400

    matched count

--- cpp/core/common.h
+++ cpp/core/common.h
@@ -37,8 +37,8 @@ template <typename T> struct DataTuple {
   uint64_t col;
 //  Eigen::Matrix<T, Eigen::Dynamic, 1> value;
 //  DataTuple(int size) : col(0), value(size){}
-  vector<T> value;
-  DataTuple() : col(0), value(std::vector<T>(10)) {}
+//  vector<T> value;
+//  DataTuple() : col(0), value(std::vector<T>(10)) {}
 };
 
 
@@ -98,23 +98,23 @@ template <typename T> void initialize_mpi_datatype_SPTUPLE() {
 
 template <typename T>
 void initialize_mpi_datatype_DENSETUPLE(int size) {
-  MPI_Datatype vectorType;
-  MPI_Type_contiguous(size, MPI_DOUBLE, &vectorType);
-  MPI_Type_commit(&vectorType);
+//  MPI_Datatype vectorType;
+//  MPI_Type_contiguous(size, MPI_DOUBLE, &vectorType);
+//  MPI_Type_commit(&vectorType);
 
-  const int nitems = 2;
-  int blocklengths[2] = {1, 1};
-  MPI_Datatype *types = new MPI_Datatype[2];
+  const int nitems = 1;
+  int blocklengths[1] = {1, 1};
+  MPI_Datatype *types = new MPI_Datatype[1];
   types[0] = MPI_UINT64_T;
-  types[1] = vectorType;
+//  types[1] = vectorType;
 
-  MPI_Aint offsets[2];
+  MPI_Aint offsets[1];
 
   DataTuple<T> dummyTuple; // Dummy struct to get displacements
 
   MPI_Get_address(&dummyTuple.col, &offsets[0]);
-  MPI_Get_address(&dummyTuple.value, &offsets[1]);
-  offsets[1] -= offsets[0];
+//  MPI_Get_address(&dummyTuple.value, &offsets[1]);
+//  offsets[1] -= offsets[0];
   offsets[0] = 0;
   cout << "offsets[0]  " << offsets[0] << " offsets[1] " << offsets[1]
        << endl;
