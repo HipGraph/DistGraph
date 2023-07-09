95f07486010946a9e4710681de23b639159ff929
Isuru Ranawaka <irjanith@gmail.com>
2023-07-09 16:06:03 -0400

    matched count

--- cpp/embedding/algo.hpp
+++ cpp/embedding/algo.hpp
@@ -129,14 +129,14 @@ public:
             (this->dense_local)->fetch_local_eigen_vector(row_id);
 
 
-        Eigen::Matrix<DENT, 1, embedding_dim> t = row_vec - col_vec;
-        Eigen::Matrix<DENT, 1, embedding_dim> t_squared = t.array().pow(2);
-        DENT t_squared_sum = t_squared.sum();
-        DENT d1 = -2.0 / (1.0 + t_squared_sum);
-        Eigen::Matrix<DENT, 1, embedding_dim> scaled_vector = t * d1;
-        Eigen::Matrix<DENT, 1, embedding_dim> clamped_vector =
-            scaled_vector.array().cwiseMax(this->MIN_BOUND).cwiseMin(this->MAX_BOUND);
-        Eigen::Matrix<DENT, 1, embedding_dim> learned = clamped_vector * lr;
+//        Eigen::Matrix<DENT, 1, embedding_dim> t = row_vec - col_vec;
+//        Eigen::Matrix<DENT, 1, embedding_dim> t_squared = t.array().pow(2);
+//        DENT t_squared_sum = t_squared.sum();
+//        DENT d1 = -2.0 / (1.0 + t_squared_sum);
+//        Eigen::Matrix<DENT, 1, embedding_dim> scaled_vector = t * d1;
+//        Eigen::Matrix<DENT, 1, embedding_dim> clamped_vector =
+//            scaled_vector.array().cwiseMax(this->MIN_BOUND).cwiseMin(this->MAX_BOUND);
+//        Eigen::Matrix<DENT, 1, embedding_dim> learned = clamped_vector * lr;
 //        values.row(i) = values.row(i).array() + learned.array();
       }
     }
