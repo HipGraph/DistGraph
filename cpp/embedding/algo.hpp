63c4c2e871505e06f05689563948b5b957e0f518
Isuru Ranawaka <irjanith@gmail.com>
2023-07-09 14:11:30 -0400

    matched count

--- cpp/embedding/algo.hpp
+++ cpp/embedding/algo.hpp
@@ -16,7 +16,7 @@ using namespace Eigen;
 
 namespace distblas::embedding {
 template <typename SPT, typename DENT, size_t embedding_dim,
-          size_t  MIN_BOUND, size_t  MAX_BOUND>
+          DENT  MIN_BOUND, DENT  MAX_BOUND>
 
 class EmbeddingAlgo {
 
