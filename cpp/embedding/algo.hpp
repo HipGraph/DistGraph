fc72ea85fdf1a9b7c88d8ffb90f6bb28ac7ca389
Isuru Ranawaka <irjanith@gmail.com>
2023-07-10 13:25:27 -0400

    matched count

--- cpp/embedding/algo.hpp
+++ cpp/embedding/algo.hpp
@@ -153,43 +153,43 @@ public:
 
     int row_base_index = batch_id * batch_size;
 
-    #pragma omp parallel for schedule(static)
+    #pragma omp parallel for
     for (int i = 0; i < values.rows(); i++) {
       uint64_t row_id = static_cast<uint64_t>(i + row_base_index);
-      for (int j = 0; j < col_ids.size(); j++) {
-        uint64_t global_col_id = col_ids[j];
-        uint64_t local_col_id =
-            global_col_id -
-            static_cast<uint64_t>(
-                ((this->grid)->global_rank * (this->sp_local)->proc_row_width));
-        bool fetch_from_cache = false;
-
-        int owner_rank = static_cast<int>(global_col_id /(this->sp_local)->proc_row_width);
-        if (owner_rank != (this->grid)->global_rank) {
-          fetch_from_cache = true;
-        }
-        Eigen::Matrix<DENT, 1, embedding_dim> col_vec;
-        if (fetch_from_cache) {
-          Eigen::Matrix<DENT, embedding_dim, 1> col_vec_trans =
-              (this->dense_local)
-                  ->fetch_data_vector_from_cache(owner_rank, global_col_id);
-          col_vec = col_vec_trans.transpose();
-        } else {
-          col_vec = (this->dense_local)->fetch_local_eigen_vector(local_col_id);
-        }
-        Eigen::Matrix<DENT, 1, embedding_dim> row_vec =
-            (this->dense_local)->fetch_local_eigen_vector(row_id);
-
-        Eigen::Matrix<DENT, 1, embedding_dim> t = row_vec - col_vec;
-        Eigen::Matrix<DENT, 1, embedding_dim> t_squared = t.array().pow(2);
-        DENT t_squared_sum = t_squared.sum();
-        DENT d1 = 2.0 / (t_squared_sum * (1.0 + t_squared_sum));
-        Eigen::Matrix<DENT, 1, embedding_dim> scaled_vector = t * d1;
-        Eigen::Matrix<DENT, 1, embedding_dim> clamped_vector =
-            scaled_vector.array().cwiseMax(this->MIN_BOUND).cwiseMin(this->MAX_BOUND);
-        Eigen::Matrix<DENT, 1, embedding_dim> learned = clamped_vector * lr;
-        values.row(i) = values.row(i).array() + learned.array();
-      }
+//      for (int j = 0; j < col_ids.size(); j++) {
+//        uint64_t global_col_id = col_ids[j];
+//        uint64_t local_col_id =
+//            global_col_id -
+//            static_cast<uint64_t>(
+//                ((this->grid)->global_rank * (this->sp_local)->proc_row_width));
+//        bool fetch_from_cache = false;
+//
+//        int owner_rank = static_cast<int>(global_col_id /(this->sp_local)->proc_row_width);
+//        if (owner_rank != (this->grid)->global_rank) {
+//          fetch_from_cache = true;
+//        }
+//        Eigen::Matrix<DENT, 1, embedding_dim> col_vec;
+//        if (fetch_from_cache) {
+//          Eigen::Matrix<DENT, embedding_dim, 1> col_vec_trans =
+//              (this->dense_local)
+//                  ->fetch_data_vector_from_cache(owner_rank, global_col_id);
+//          col_vec = col_vec_trans.transpose();
+//        } else {
+//          col_vec = (this->dense_local)->fetch_local_eigen_vector(local_col_id);
+//        }
+//        Eigen::Matrix<DENT, 1, embedding_dim> row_vec =
+//            (this->dense_local)->fetch_local_eigen_vector(row_id);
+//
+//        Eigen::Matrix<DENT, 1, embedding_dim> t = row_vec - col_vec;
+//        Eigen::Matrix<DENT, 1, embedding_dim> t_squared = t.array().pow(2);
+//        DENT t_squared_sum = t_squared.sum();
+//        DENT d1 = 2.0 / (t_squared_sum * (1.0 + t_squared_sum));
+//        Eigen::Matrix<DENT, 1, embedding_dim> scaled_vector = t * d1;
+//        Eigen::Matrix<DENT, 1, embedding_dim> clamped_vector =
+//            scaled_vector.array().cwiseMax(this->MIN_BOUND).cwiseMin(this->MAX_BOUND);
+//        Eigen::Matrix<DENT, 1, embedding_dim> learned = clamped_vector * lr;
+//        values.row(i) = values.row(i).array() + learned.array();
+//      }
     }
   }
 
