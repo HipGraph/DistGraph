15e2a53fae1203a6f9c6139fddcd8aeb5ffbdaee
Isuru Ranawaka <irjanith@gmail.com>
2023-07-10 14:07:58 -0400

    matched count

--- cpp/core/csr_linked_list.hpp
+++ cpp/core/csr_linked_list.hpp
@@ -14,7 +14,7 @@ struct CSRLocalNode {
   shared_ptr<CSRLocal<T>> data = nullptr;
   shared_ptr<CSRLocalNode<T>> next = nullptr;
 
-  uint64_t id;
+  int total_nodes;
 };
 
 template <typename T>
@@ -30,6 +30,7 @@ public:
   CSRLinkedList(int num_of_nodes) {
     head = nullptr;
     direct_ref = vector<shared_ptr<CSRLocal<T>>>(num_of_nodes);
+    total_nodes = num_of_nodes;
   }
 
   ~CSRLinkedList() {
@@ -43,7 +44,8 @@ public:
     newNode.get()->id=id;
     newNode.get()->data = make_shared<CSRLocal<T>>( rows,  cols,  max_nnz,  coords,
                                                    num_coords,  transpose);
-    direct_ref[id]= (newNode.get()->data);
+    int index = id %total_nodes;
+    direct_ref[index]= (newNode.get()->data);
     if (this->head == nullptr) {
       head = newNode;
     }else {
