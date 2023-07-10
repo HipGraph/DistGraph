afce17a8ab54dc89e52de3ad37057b3ee7196e39
Isuru Ranawaka <irjanith@gmail.com>
2023-07-10 10:05:49 -0400

    matched count

--- cpp/core/sparse_mat.hpp
+++ cpp/core/sparse_mat.hpp
@@ -176,11 +176,11 @@ public:
       int num_coords = block_row_starts[j + 1] - block_row_starts[j];
       int rank;
       MPI_Comm_rank(MPI_COMM_WORLD, &rank);
-
-      cout << " rank " << rank << "_" << transpose
-           << " csr_block_initating_index" << block_row_starts[j]
-           << " current vec pos" << current_vector_pos << " col_block"
-           << col_block << endl;
+//
+//      cout << " rank " << rank << "_" << transpose
+//           << " csr_block_initating_index" << block_row_starts[j]
+//           << " current vec pos" << current_vector_pos << " col_block"
+//           << col_block << endl;
 
       Tuple<T> *coords_ptr = (coords.data() + block_row_starts[j]);
       (csr_linked_lists[current_vector_pos].get())
