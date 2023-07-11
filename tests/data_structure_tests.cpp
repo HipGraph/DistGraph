64d6c6950a679f2f55caee526cf01ee655d2c368
Isuru Ranawaka <irjanith@gmail.com>
2023-07-11 12:47:10 -0400

    matched count

--- tests/data_structure_tests.cpp
+++ tests/data_structure_tests.cpp
@@ -106,8 +106,8 @@ int main(int argc, char **argv) {
 //
   shared_sparseMat_combined.get()->divide_block_cols(
       localBRows, localBRows, grid.get()->world_size, true, false);
-  shared_sparseMat_combined.get()->sort_by_rows();
-  shared_sparseMat_combined.get()->divide_block_rows(300, localBRows, true, false);
+//  shared_sparseMat_combined.get()->sort_by_rows();
+//  shared_sparseMat_combined.get()->divide_block_rows(300, localBRows, true, false);
 
 
 
