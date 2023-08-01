8d8aa0c3d20a6d5564d30499593dbfd6078f2725
Isuru Ranawaka <irjanith@gmail.com>
2023-08-01 17:43:34 -0400

    training duration

--- cpp/embedding/algo.hpp
+++ cpp/embedding/algo.hpp
@@ -291,7 +291,7 @@ public:
             DENT d1 = -2.0 / (1.0 + attrc);
             for (int d = 0; d < embedding_dim; d++) {
               forceDiff[d] = scale(forceDiff[d] * d1);
-              prevCoordinates[i] += (lr)*forceDiff[d];
+              prevCoordinates[i+d] += (lr)*forceDiff[d];
             }
           }
         }
@@ -344,7 +344,7 @@ public:
                            (1.0 + repuls));
           for (int d = 0; d < embedding_dim; d++) {
             forceDiff[d] = scale(forceDiff[d] * d1);
-            prevCoordinates[i] += (lr)*forceDiff[d];
+            prevCoordinates[i+d] += (lr)*forceDiff[d];
           }
 
 
