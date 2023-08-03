86b8e914145b8f4bd7c369a54d40f59263bf1734
Isuru Ranawaka <irjanith@gmail.com>
2023-08-03 14:09:29 -0400

    training duration

--- cpp/core/sparse_mat.hpp
+++ cpp/core/sparse_mat.hpp
@@ -281,23 +281,23 @@ public:
         int proc_col_width = this->proc_col_width;
         distblas::core::CSRHandle *handle = (csr_data.get())->handler.get();
         col_ids = vector<uint64_t>((handle->col_idx).size());
-//        std::transform(
-//            std::begin((handle->col_idx)), std::end((handle->col_idx)),
-//            std::begin(col_ids),
-//            [&return_global_ids, &rank, &transpose, &batch_id, &block_col_id,
-//             &block_row_width, &block_col_width, &proc_col_width,
-//             &proc_row_width](MKL_INT value) {
-//              if (!return_global_ids) {
-//                return static_cast<uint64_t>(value);
-//              } else {
-//                int starting_index = (transpose) ? rank * proc_col_width : 0;
-//                uint64_t base_id =
-//                    static_cast<uint64_t>(block_col_id * block_col_width);
-//                uint64_t g_index = static_cast<uint64_t>(value) + base_id +
-//                                   static_cast<uint64_t>(starting_index);
-//                return g_index;
-//              }
-//            });
+        std::transform(
+            std::begin((handle->col_idx)), std::end((handle->col_idx)),
+            std::begin(col_ids),
+            [&return_global_ids, &rank, &transpose, &batch_id, &block_col_id,
+             &block_row_width, &block_col_width, &proc_col_width,
+             &proc_row_width](MKL_INT value) {
+              if (!return_global_ids) {
+                return static_cast<uint64_t>(value);
+              } else {
+                int starting_index = (transpose) ? rank * proc_col_width : 0;
+                uint64_t base_id =
+                    static_cast<uint64_t>(block_col_id * block_col_width);
+                uint64_t g_index = static_cast<uint64_t>(value) + base_id +
+                                   static_cast<uint64_t>(starting_index);
+                return g_index;
+              }
+            });
       }
     }
   }
