851fa9d4334848729211bb6a896408f932d75184
Isuru Ranawaka <irjanith@gmail.com>
2023-08-03 13:57:52 -0400

    training duration

--- cpp/net/data_comm.hpp
+++ cpp/net/data_comm.hpp
@@ -102,29 +102,29 @@ public:
 
       // calculating sending data cols
 
-      for (int i = 0; i < no_of_lists_trans; i++) {
-        int working_rank = 0;
-
-        for (int j = 0; j < total_nodes_trans; j++) {
-          if (j > 0 and j % no_of_nodes_per_proc_list_trans == 0) {
-            ++working_rank;
-          }
-          if (working_rank != grid->global_rank) {
-            vector<uint64_t> col_ids;
-            this->sp_local_trans->fill_col_ids(j, i, col_ids, true, true);
-            send_col_ids_list[working_rank].insert(
-                send_col_ids_list[working_rank].end(), col_ids.begin(),
-                col_ids.end());
-            std::unordered_set<MKL_INT> unique_set(
-                send_col_ids_list[working_rank].begin(),
-                send_col_ids_list[working_rank].end());
-            send_col_ids_list[working_rank] =
-                vector<uint64_t>(unique_set.begin(), unique_set.end());
-            //
-          }
-          sendcounts[working_rank] = send_col_ids_list[working_rank].size();
-        }
-      }
+//      for (int i = 0; i < no_of_lists_trans; i++) {
+//        int working_rank = 0;
+//
+//        for (int j = 0; j < total_nodes_trans; j++) {
+//          if (j > 0 and j % no_of_nodes_per_proc_list_trans == 0) {
+//            ++working_rank;
+//          }
+//          if (working_rank != grid->global_rank) {
+//            vector<uint64_t> col_ids;
+//            this->sp_local_trans->fill_col_ids(j, i, col_ids, true, true);
+//            send_col_ids_list[working_rank].insert(
+//                send_col_ids_list[working_rank].end(), col_ids.begin(),
+//                col_ids.end());
+//            std::unordered_set<MKL_INT> unique_set(
+//                send_col_ids_list[working_rank].begin(),
+//                send_col_ids_list[working_rank].end());
+//            send_col_ids_list[working_rank] =
+//                vector<uint64_t>(unique_set.begin(), unique_set.end());
+//            //
+//          }
+//          sendcounts[working_rank] = send_col_ids_list[working_rank].size();
+//        }
+//      }
 
 //      for (int i = 0; i < grid->world_size; i++) {
 //
