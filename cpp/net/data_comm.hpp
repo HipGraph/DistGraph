d3cc2aeb4e808db99c553587b41bee87acd65ff5
Isuru Ranawaka <irjanith@gmail.com>
2023-08-04 08:19:05 -0400

    training duration

--- cpp/net/data_comm.hpp
+++ cpp/net/data_comm.hpp
@@ -167,7 +167,7 @@ public:
           if (j == working_rank * no_of_nodes_per_proc_list + offset) {
             if (working_rank != grid->global_rank) {
               vector<uint64_t> col_ids;
-//              this->sp_local->fill_col_ids(i, j, col_ids, false, true);
+              this->sp_local->fill_col_ids(i, j, col_ids, false, true);
               //
               //              receive_col_ids_list[working_rank].insert(
               //                  receive_col_ids_list[working_rank].end(),
