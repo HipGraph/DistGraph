a8bcaaad00b49219a584fa15d70403d987667ec1
Isuru Ranawaka <irjanith@gmail.com>
2023-08-05 18:35:51 -0400

    training duration

--- tests/data_structure_tests.cpp
+++ tests/data_structure_tests.cpp
@@ -146,7 +146,7 @@ int main(int argc, char **argv) {
 
   auto end_init = std::chrono::high_resolution_clock::now();
 
-  embedding_algo.get()->algo_force2_vec_ns(9600, 300, 5, 0.02);
+  embedding_algo.get()->algo_force2_vec_ns(1200, 600, 5, 0.02);
 
   auto end_train = std::chrono::high_resolution_clock::now();
   //  cout << " rank " << rank << " async completed  " << endl;
