135cd1f1ce57e65da96ad062530a69e4b6750811
Isuru Ranawaka <irjanith@gmail.com>
2023-08-05 15:45:36 -0400

    training duration

--- cpp/core/dense_mat.hpp
+++ cpp/core/dense_mat.hpp
@@ -102,7 +102,7 @@ public:
 //    fout << (*this->matrixPtr).rows() << " " << (*this->matrixPtr).cols()
 //         << endl;
     for (int i = 0; i < (*this->matrixPtr).rows(); ++i) {
-      fout << i + 1 rank*rows<< " ";
+      fout << i + 1+ rank*rows<< " ";
       for (int j = 0; j < embedding_dim; ++j) {
         fout << this->nCoordinates[i * embedding_dim + j] << " ";
       }
