9cf157d9dada6a19b0696960fd0ef38d1cc5d2d8
Isuru Ranawaka <irjanith@gmail.com>
2023-08-05 22:46:14 -0400

    training duration

--- cpp/embedding/algo.hpp
+++ cpp/embedding/algo.hpp
@@ -216,8 +216,9 @@ public:
       for (int i = 0; i < block_size; i++) {
         uint64_t row_id = static_cast<uint64_t>(i + row_base_index);
         DENT forceDiff[embedding_dim];
-        cout<<" for loop starting completed for i"<<i<<" outof "<<block_size<<endl;
-        cout<<" executing csr_handle_j<<"<<static_cast<uint64_t>(csr_handle->rowStart[i])<<endl;
+        cout<<i<<"i "<<<" for loop starting completed for i"<<i<<" outof "<<block_size<<endl;
+        cout<<i<<" i "<<<" executing csr_handle_j<<"<<static_cast<uint64_t>(csr_handle->rowStart[i])<<endl;
+        cout<<i<<" i "<<<" executing end csr_handle_j<<"<<static_cast<uint64_t>(csr_handle->rowStart[i+1])<<endl;
         //#pragma forceinline
         //#pragma omp simd
         for (uint64_t j = static_cast<uint64_t>(csr_handle->rowStart[i]);
