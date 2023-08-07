9807e4577ff24602828397e50f45df7d3747c876
Isuru Ranawaka <irjanith@gmail.com>
2023-08-07 17:27:00 -0400

    training duration

--- cpp/partition/partitioner.hpp
+++ cpp/partition/partitioner.hpp
@@ -43,6 +43,8 @@ public:
     int considered_row_width;
     if (my_rank == world_size - 1) {
       considered_row_width = sp_mat->gRows - sp_mat->proc_row_width *my_rank ;
+    }else{
+      considered_row_width = sp_mat->proc_row_width;
     }
 
     Tuple<T> *sendbuf = new Tuple<T>[sp_mat->coords.size()];
