be86f242c4d6efd1cc390caf771df01b0d4f4ec0
Isuru Ranawaka <irjanith@gmail.com>
2023-08-07 19:40:10 -0400

    training duration

--- cpp/embedding/algo.hpp
+++ cpp/embedding/algo.hpp
@@ -61,6 +61,19 @@ public:
 
   void algo_force2_vec_ns(int iterations, int batch_size, int ns, DENT lr) {
     int batches = ((this->dense_local)->rows / batch_size);
+    int last_batch_size=0;
+
+    int batches = 0;
+    if (sp_local->proc_row_width %batch_size == 0) {
+      batches =
+          std::max(1, static_cast<int>(sp_local->proc_row_width / batch_size));
+      last_batch_size = batch_size;
+    } else {
+      batches =
+          std::max(1, static_cast<int>(sp_local->proc_row_width / batch_size))+1; //TODO:Error prone
+      last_batch_size = (this->dense_local)->rows - batch_size*batches;
+    }
+
 
     cout << " rank " << this->grid->global_rank << " total batches " << batches << endl;
 
@@ -160,22 +173,29 @@ public:
         bool fetch_remote =
             (working_rank == ((this->grid)->global_rank)) ? false : true;
 
-//        cout<<" rank  "<<this->grid->global_rank<<"executiong first row updatefor batch "<<j<<" batch_size "<<batch_size<<endl;
-        this->calc_t_dist_grad_rowptr(csr_block_local, prevCoordinates, lr, j,
-                                      batch_size, batch_size);
-//        cout<<" rank  "<<this->grid->global_rank<<"executiong first row updatefor batch "<<j<<" completed "<<endl;
+        if (j==batches-1){
+          this->calc_t_dist_grad_rowptr(csr_block_local, prevCoordinates, lr, j,
+                                        batch_size, last_batch_size);
+          this->calc_t_dist_replus_rowptr(prevCoordinates, random_number_vec,
+                                          lr, j, batch_size, last_batch_size);
+          if (this->grid->world_size > 1) {
 
-        this->calc_t_dist_replus_rowptr(prevCoordinates,
-               random_number_vec, lr,
-                                                j, batch_size, batch_size);
+            this->calc_t_dist_grad_rowptr(csr_block_remote, prevCoordinates, lr,
+                                          j, batch_size, last_batch_size);
+          }
+          this->update_data_matrix_rowptr(prevCoordinates, j,last_batch_size);
+        }else{
+          this->calc_t_dist_grad_rowptr(csr_block_local, prevCoordinates, lr, j,
+                                        batch_size, batch_size);
+          this->calc_t_dist_replus_rowptr(prevCoordinates, random_number_vec,
+                                          lr, j, batch_size, batch_size);
+          if (this->grid->world_size > 1) {
 
-        if (this->grid->world_size > 1) {
-
-          this->calc_t_dist_grad_rowptr(csr_block_remote, prevCoordinates, lr,
-                                        j, batch_size, batch_size);
+            this->calc_t_dist_grad_rowptr(csr_block_remote, prevCoordinates, lr,
+                                          j, batch_size, batch_size);
+          }
+          this->update_data_matrix_rowptr(prevCoordinates, j,batch_size);
         }
-
-        this->update_data_matrix_rowptr(prevCoordinates, j,batch_size);
 
         if (this->grid->world_size > 1) {
           MPI_Request request_three;
