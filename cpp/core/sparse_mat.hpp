71282b30b25761ec697e86311ac1fd58e45b641e
Isuru Ranawaka <irjanith@gmail.com>
2023-08-09 13:41:31 -0400

    change batch_size to 170

--- cpp/core/sparse_mat.hpp
+++ cpp/core/sparse_mat.hpp
@@ -312,7 +312,7 @@ public:
               next_start += batch_size;
             }
           }
-          if (rank == 3 and i=22 and trans) {
+          if (rank == 3 and i==22 and trans) {
             cout << " current row start: " << current_start<< " size: " << matched_count << endl;}
         }
 
