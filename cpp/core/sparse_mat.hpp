36bf7f357ddcc6041cd220510ed99e7d40788f2a
Isuru Ranawaka <irjanith@gmail.com>
2023-08-09 09:57:37 -0400

    training duration

--- cpp/core/sparse_mat.hpp
+++ cpp/core/sparse_mat.hpp
@@ -462,7 +462,11 @@ public:
         distblas::core::CSRHandle *handle =
                 (csr_data.get())->handler.get();
         ////        col_ids = vector<uint64_t>((handle->col_idx).size());
-                col_ids = vector<uint64_t>((handle->values).size());
+        cout<< " rank " <<rank << " values size  "<<
+            "("<<block_row_id<<","<<block_col_id<<")"<<(handle->values).size()<<endl;
+
+
+        col_ids = vector<uint64_t>((handle->values).size());
         //        std::transform(
         //            std::begin((handle->col_idx)),
         //            std::end((handle->col_idx)), std::begin(col_ids),
