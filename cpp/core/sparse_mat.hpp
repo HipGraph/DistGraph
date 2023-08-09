c23bf0bac40779d972f117e5197ed866b2c54895
Isuru Ranawaka <irjanith@gmail.com>
2023-08-09 14:02:00 -0400

    change batch_size to 170

--- cpp/core/sparse_mat.hpp
+++ cpp/core/sparse_mat.hpp
@@ -271,10 +271,14 @@ public:
         while (coords[j].row >= current_start) {
           block_row_starts.push_back(j);
           ++matched_count;
+          if (rank == 3 and i==22 and trans) {
+            cout << " current row start: " << current_start<< " size: " << matched_count<<" " << j<<" "<<coords[j].row << endl;}
           if (coords[j].row > next_start) {
             while (coords[j].row > next_start) {
               block_row_starts.push_back(j);
               ++matched_count;
+              if (rank == 3 and i==22 and trans) {
+                cout << " current row start: " << current_start<< " size: " << matched_count<<" " << j<<" "<<coords[j].row << endl;}
               if (!divided_equallaly) {
                 if (j > block_col_starts[i] and
                     (matched_count) % (batch_count) == 0) {
@@ -312,8 +316,7 @@ public:
               next_start += batch_size;
             }
           }
-          if (rank == 3 and i==22 and trans) {
-            cout << " current row start: " << current_start<< " size: " << matched_count<<" " << j<<" "<<coords[j].row << endl;}
+
         }
 
         // This modding step helps indexing.
