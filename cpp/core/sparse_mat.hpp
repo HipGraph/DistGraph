ed8be737af87ff499003b39936c8ab494a4826e4
Isuru Ranawaka <irjanith@gmail.com>
2023-08-09 09:17:43 -0400

    training duration

--- cpp/core/sparse_mat.hpp
+++ cpp/core/sparse_mat.hpp
@@ -440,48 +440,48 @@ public:
       head = (head.get())->next;
       ++count;
     }
-    if (count == batch_id) {
-      auto csr_data = (head.get())->data;
-      //      cout << " rank  " << rank << " inside fill_col_ids coords ( "
-      //           << block_row_id << " ," << block_col_id << ")"
-      //           << (csr_data.get())->num_coords << endl;
-      if ((csr_data.get())->num_coords > 0) {
-        int block_row_width = this->block_row_width;
-        int block_col_width = this->block_col_width;
-        int proc_row_width = this->proc_row_width;
-        int proc_col_width = this->proc_col_width;
-        distblas::core::CSRHandle *handle = (csr_data.get())->handler.get();
-//        col_ids = vector<uint64_t>((handle->col_idx).size());
-        col_ids = vector<uint64_t>((handle->values).size());
-//        std::transform(
-//            std::begin((handle->col_idx)), std::end((handle->col_idx)),
-//            std::begin(col_ids),
-//            [&return_global_ids, &rank, &transpose, &batch_id, &block_col_id,
-//             &block_row_width, &block_col_width, &proc_col_width,
-//             &proc_row_width](MKL_INT value) {
-//              if (!return_global_ids) {
-//                return static_cast<uint64_t>(value);
-//              } else {
-//                int starting_index = (transpose) ? rank * proc_col_width : 0;
-//                uint64_t base_id =
-//                    static_cast<uint64_t>(block_col_id * block_col_width);
-//                uint64_t g_index = static_cast<uint64_t>(value) + base_id +
-//                                   static_cast<uint64_t>(starting_index);
-//                //TODO: do proper transformation here
-//                return g_index;
-//              }
-//            });
-
-//        std::transform(
-//            std::begin((handle->values)), std::end((handle->values)),
-//            std::begin(col_ids),
-//            [&return_global_ids, &rank, &transpose, &batch_id, &block_col_id,
-//             &block_row_width, &block_col_width, &proc_col_width,
-//             &proc_row_width](double value) {
-//              return static_cast<uint64_t>(value);
-//            });
-      }
-    }
+//    if (count == batch_id) {
+//      auto csr_data = (head.get())->data;
+//      //      cout << " rank  " << rank << " inside fill_col_ids coords ( "
+//      //           << block_row_id << " ," << block_col_id << ")"
+//      //           << (csr_data.get())->num_coords << endl;
+//      if ((csr_data.get())->num_coords > 0) {
+//        int block_row_width = this->block_row_width;
+//        int block_col_width = this->block_col_width;
+//        int proc_row_width = this->proc_row_width;
+//        int proc_col_width = this->proc_col_width;
+//        distblas::core::CSRHandle *handle = (csr_data.get())->handler.get();
+////        col_ids = vector<uint64_t>((handle->col_idx).size());
+//        col_ids = vector<uint64_t>((handle->values).size());
+////        std::transform(
+////            std::begin((handle->col_idx)), std::end((handle->col_idx)),
+////            std::begin(col_ids),
+////            [&return_global_ids, &rank, &transpose, &batch_id, &block_col_id,
+////             &block_row_width, &block_col_width, &proc_col_width,
+////             &proc_row_width](MKL_INT value) {
+////              if (!return_global_ids) {
+////                return static_cast<uint64_t>(value);
+////              } else {
+////                int starting_index = (transpose) ? rank * proc_col_width : 0;
+////                uint64_t base_id =
+////                    static_cast<uint64_t>(block_col_id * block_col_width);
+////                uint64_t g_index = static_cast<uint64_t>(value) + base_id +
+////                                   static_cast<uint64_t>(starting_index);
+////                //TODO: do proper transformation here
+////                return g_index;
+////              }
+////            });
+//
+////        std::transform(
+////            std::begin((handle->values)), std::end((handle->values)),
+////            std::begin(col_ids),
+////            [&return_global_ids, &rank, &transpose, &batch_id, &block_col_id,
+////             &block_row_width, &block_col_width, &proc_col_width,
+////             &proc_row_width](double value) {
+////              return static_cast<uint64_t>(value);
+////            });
+//      }
+//    }
     //    }
   }
 
