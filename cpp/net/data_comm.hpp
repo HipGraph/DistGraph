77c421a2c6deb4a95b1675d40af07f8f49f60c7a
Isuru Ranawaka <irjanith@gmail.com>
2023-08-08 22:06:36 -0400

    training duration

--- cpp/net/data_comm.hpp
+++ cpp/net/data_comm.hpp
@@ -100,7 +100,13 @@ public:
           }
           if (working_rank != grid->global_rank) {
             vector<uint64_t> col_ids;
+            if (grid->global_rank==13){
+              cout<<" Accessing i "<<i<<" j "<<j<<endl;
+            }
             this->sp_local->fill_col_ids(i, j, col_ids, false, true);
+            if (grid->global_rank==13){
+              cout<<" Success Accessing i "<<i<<" j "<<j<<endl;
+            }
             receive_col_ids_list[working_rank].insert(
                 receive_col_ids_list[working_rank].end(), col_ids.begin(),
                 col_ids.end());
