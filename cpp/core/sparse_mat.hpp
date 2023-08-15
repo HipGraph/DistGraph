0efb2f082f6871e50da5b2ce9cda0f9b98cdea97
Isuru Ranawaka <irjanith@gmail.com>
2023-08-15 18:24:58 -0400

    simplifying code

--- cpp/core/sparse_mat.hpp
+++ cpp/core/sparse_mat.hpp
@@ -447,7 +447,7 @@ public:
 //      }
 
       (csr_linked_lists[0].get())
-          ->insert((transpose)?gRows:block_rows, (transpose)?block_cols:gCols, coords.size(), coords_ptr,  coords.size(), transpose,
+          ->insert((transpose)?block_cols:block_rows, (transpose)?gRows:gCols, coords.size(), coords_ptr,  coords.size(), transpose,
                    node_index);
 //    }
   }
