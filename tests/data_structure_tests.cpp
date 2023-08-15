79620db94e6d1c31e0a5e42de09d27589069c842
Isuru Ranawaka <irjanith@gmail.com>
2023-08-15 10:25:41 -0400

    change batch_size to 170

--- tests/data_structure_tests.cpp
+++ tests/data_structure_tests.cpp
@@ -23,13 +23,15 @@ int main(int argc, char **argv) {
 
   cout << " file_path " << file_path << endl;
 
-  int batch_size = 170;
+  int batch_size = 256;
+
 
   MPI_Init(&argc, &argv);
   int rank;
   int world_size;
   MPI_Comm_rank(MPI_COMM_WORLD, &rank);
   MPI_Comm_size(MPI_COMM_WORLD, &world_size);
+   batch_size = batch_size/world_size;
 
   // Initialize MPI DataTypes
   initialize_mpi_datatypes<int, double, 2>();
