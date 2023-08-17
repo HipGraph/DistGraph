a27b14c4c0954ac76838461155a435c35b44a30e
Isuru Ranawaka <irjanith@gmail.com>
2023-08-17 15:07:13 -0400

    profiling

--- cpp/embedding/algo.hpp
+++ cpp/embedding/algo.hpp
@@ -85,7 +85,7 @@ public:
         ::operator new(sizeof(DENT[batch_size * embedding_dim])));
 
     for (int i = 0; i < 1; i++) {
-      for (int j = 0; j < 1; j++) {
+      for (int j = 0; j < batches; j++) {
 
         int seed = j + i;
 
