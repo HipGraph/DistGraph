4b6a98a1592854cf1497c3989801efdd5e310bec
Isuru Ranawaka <irjanith@gmail.com>
2023-08-18 07:45:09 -0400

    profiling

--- cpp/partition/partitioner.hpp
+++ cpp/partition/partitioner.hpp
@@ -40,12 +40,12 @@ public:
     int world_size = process_3D_grid->world_size;
     int my_rank = process_3D_grid->global_rank;
 
-    int considered_row_width;
-    if (my_rank == world_size - 1) {
-      considered_row_width = sp_mat->gRows - sp_mat->proc_row_width *my_rank ;
-    }else{
-      considered_row_width = sp_mat->proc_row_width;
-    }
+//    int considered_row_width;
+//    if (my_rank == world_size - 1) {
+//      considered_row_width = sp_mat->gRows - sp_mat->proc_row_width *my_rank ;
+//    }else{
+//      considered_row_width = sp_mat->proc_row_width;
+//    }
 
     Tuple<T> *sendbuf = new Tuple<T>[sp_mat->coords.size()];
 
@@ -62,7 +62,7 @@ public:
 #pragma omp parallel for
       for (int i = 0; i < coords.size(); i++) {
         int owner = get_owner_Process(coords[i].row, coords[i].col,
-                                      considered_row_width,
+                                      sp_mat->proc_row_width,
                                       sp_mat->proc_col_width,
                                       sp_mat->gCols,transpose);
 #pragma omp atomic update
