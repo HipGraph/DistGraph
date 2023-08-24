0a3c0262ddd016d6a10b0a7e3a804d36acce8f60
Isuru Ranawaka <irjanith@gmail.com>
2023-08-24 15:13:33 -0400

    checking randomseed based rand()

--- cpp/core/dense_mat.hpp
+++ cpp/core/dense_mat.hpp
@@ -182,7 +182,7 @@ public:
             }
 
             if (result != values.end()) {
-              this->insert_cache(i,global_index,stdArray);
+//              this->insert_cache(i,global_index,stdArray);
             }
           }
         }
