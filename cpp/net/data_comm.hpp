b76017dbb0357d382e03bb1b5c124a645861e5d8
Isuru Ranawaka <irjanith@gmail.com>
2023-08-30 15:33:04 -0400

    checking randomseed based rand()

--- cpp/net/data_comm.hpp
+++ cpp/net/data_comm.hpp
@@ -227,7 +227,7 @@ public:
       int base_index = this->rdispls[i];
       int count = this->receivecounts[i];
       for (int j = base_index; j < base_index + count; j++) {
-        DataTuple<DENT, embedding_dim> t = (*receivebuf)[j];
+//        DataTuple<DENT, embedding_dim> t = (*receivebuf)[j];
 //        (this->dense_local)->insert_cache(i, t.col, t.value);
       }
     }
