c4f801aafce9756600ac2a5b24f0f20af75f8da1
Isuru Ranawaka <irjanith@gmail.com>
2023-08-30 07:47:09 -0400

    checking randomseed based rand()

--- cpp/net/data_comm.hpp
+++ cpp/net/data_comm.hpp
@@ -138,6 +138,9 @@ public:
       MPI_Alltoallv(sendbuf, sendcounts.data(), sdispls.data(), DENSETUPLE,
                     (*receivebuf).data(), receivecounts.data(), rdispls.data(),
                     DENSETUPLE, MPI_COMM_WORLD);
+      MPI_Request dumy;
+      this->populate_cache(receivebuf, dumy, true);
+
     } else {
       MPI_Ialltoallv(sendbuf, sendcounts.data(), sdispls.data(), DENSETUPLE,
                      (*receivebuf).data(), receivecounts.data(), rdispls.data(),
