f9643ea29ffb13b8d6821452b8c9a937a01a6d23
Isuru Ranawaka <irjanith@gmail.com>
2023-08-30 23:06:14 -0400

    checking randomseed based rand()

--- cpp/algo/algo.hpp
+++ cpp/algo/algo.hpp
@@ -149,8 +149,8 @@ public:
         if (j == batches - 1) {
           considering_batch_size = last_batch_size;
         }
-//        this->calc_t_dist_replus_rowptr(prevCoordinates, random_number_vec, lr,
-//                                        j, batch_size, considering_batch_size);
+        this->calc_t_dist_replus_rowptr(prevCoordinates, random_number_vec, lr,
+                                        j, batch_size, considering_batch_size);
 
         CSRLocal<SPT> *csr_block = (this->sp_local_receiver)->csr_local_data.get();
 
@@ -168,12 +168,12 @@ public:
             t = start_clock();
           }
 
-//          this->calc_t_dist_grad_rowptr(csr_block, prevCoordinates, lr, j,
-//                                        batch_size, considering_batch_size,
-//                                        false);
+          this->calc_t_dist_grad_rowptr(csr_block, prevCoordinates, lr, j,
+                                        batch_size, considering_batch_size,
+                                        false);
           stop_clock_and_add(t, "Computation Time");
         }
-//        this->update_data_matrix_rowptr(prevCoordinates, j, batch_size);
+        this->update_data_matrix_rowptr(prevCoordinates, j, batch_size);
         update_ptr.get()->clear();
 
         if (this->grid->world_size > 1) {
