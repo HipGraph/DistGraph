98cc03d2a840d44716c449207134724367be2942
Isuru Ranawaka <irjanith@gmail.com>
2023-08-30 21:57:44 -0400

    checking randomseed based rand()

--- cpp/algo/algo.hpp
+++ cpp/algo/algo.hpp
@@ -104,10 +104,10 @@ public:
     for (int i = 0; i < batches; i++) {
       auto communicator = unique_ptr<DataComm<SPT, DENT, embedding_dim>>(
           new DataComm<SPT, DENT, embedding_dim>(
-              sp_local_receiver, sp_local_sender, dense_local, grid,0 ));
+              sp_local_receiver, sp_local_sender, dense_local, grid,-1 ));
       data_comm_cache.insert(std::make_pair(i, std::move(communicator)));
-      data_comm_cache[0].get()->onboard_data();
-      data_comm_cache[0].get()->transfer_data(fetch_all_ptr.get(), false,
+      data_comm_cache[i].get()->onboard_data();
+      data_comm_cache[i].get()->transfer_data(fetch_all_ptr.get(), false,
                                                    fetch_all);
     }
     stop_clock_and_add(t, "Computation Time");
