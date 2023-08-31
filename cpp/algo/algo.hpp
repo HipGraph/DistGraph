40d3b48d4f7802f2d1d50b8365eb4af0286e9cdd
Isuru Ranawaka <irjanith@gmail.com>
2023-08-31 10:59:44 -0400

    push updates

--- cpp/algo/algo.hpp
+++ cpp/algo/algo.hpp
@@ -290,9 +290,11 @@ public:
             DENT d1 = -2.0 / (1.0 + attrc);
 
             for (int d = 0; d < embedding_dim; d++) {
+                   cout<< forceDiff[d] * d1<<" "
 //              forceDiff[d] = scale(forceDiff[d] * d1);
 //              prevCoordinates[index * embedding_dim + d] += (lr)*forceDiff[d];
             }
+            cout<< endl;
           }
         }
       }
