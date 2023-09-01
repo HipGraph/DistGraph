ecf6a1e2bde72695d393e80a9122eec34794ae86
Isuru Ranawaka <irjanith@gmail.com>
2023-09-01 07:12:44 -0400

    push updates

--- cpp/algo/algo.hpp
+++ cpp/algo/algo.hpp
@@ -273,9 +273,9 @@ public:
 
             if (!matched) {
               if (fetch_from_cache) {
-                array_ptr =
-                    (this->dense_local)
-                        ->fetch_data_vector_from_cache_ptr(target_rank, i);
+//                array_ptr =
+//                    (this->dense_local)
+//                        ->fetch_data_vector_from_cache_ptr(target_rank, i);
                 // If not in cache we should fetch that from remote for limited
                 // cache
               }
