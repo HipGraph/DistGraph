2c8e86c6a44415209cbfc765143b5ab9744d0bec
Isuru Ranawaka <irjanith@gmail.com>
2023-09-03 08:36:36 -0400

    push updates

--- cpp/algo/algo.hpp
+++ cpp/algo/algo.hpp
@@ -221,6 +221,7 @@ public:
 //      calc_embedding(source_start_index, source_end_index, dst_start_index,
 //                     dst_end_index, csr_block, prevCoordinates, lr, batch_id,
 //                     batch_size, block_size);
+
             calc_embedding_row_major(
                 source_start_index, source_end_index, dst_start_index,
                 dst_end_index, csr_block, prevCoordinates, lr, batch_id,
@@ -237,6 +238,7 @@ public:
 //          calc_embedding(source_start_index, source_end_index, dst_start_index,
 //                         dst_end_index, csr_block, prevCoordinates, lr,
 //                         batch_id, batch_size, block_size);
+
                     calc_embedding_row_major(source_start_index,
                     source_end_index,
                                              dst_start_index, dst_end_index,
