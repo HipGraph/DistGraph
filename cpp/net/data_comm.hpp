95d6d95cbafd3d6f6a90681442df18c34dc2d8b6
Isuru Ranawaka <irjanith@gmail.com>
2023-09-05 15:18:53 -0400

    checking communication

--- cpp/net/data_comm.hpp
+++ cpp/net/data_comm.hpp
@@ -262,9 +262,9 @@ public:
     for (int i = 0; i < grid->world_size; i++) {
       total_receive_count +=receivecounts_misses[i];
       rdisples_misses[i]= (i>0)?rdisples_misses[i-1]+receivecounts_misses[i-1]:rdisples_misses[i];
-      if (grid->world_size==0){
+//      if (grid->world_size==0){
         cout<<" rank "<<grid->global_rank<<" receiving count  from rank"<<i<<receivecounts_misses[i]<<endl;
-      }
+//      }
 
     }
     unique_ptr<vector<DataTuple<DENT, embedding_dim>>> receive_missing_cols_ptr =
