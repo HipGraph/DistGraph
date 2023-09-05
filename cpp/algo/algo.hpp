45c1af04215f593eaa3dc051b35dbb991ace459f
Isuru Ranawaka <irjanith@gmail.com>
2023-09-05 12:53:18 -0400

    checking communication

--- cpp/algo/algo.hpp
+++ cpp/algo/algo.hpp
@@ -332,7 +332,7 @@ public:
                   Tuple<DENT> cacheRef;
                   cacheRef.row = source_id;
                   cacheRef.col = i;
-                  (*cache_misses)[target_rank].push_back(cacheRef);
+                  (*cache_misses)[target_rank].push_back(cacheRef.col);
                   continue;
                 }
               }
