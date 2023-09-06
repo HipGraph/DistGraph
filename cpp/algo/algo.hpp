ad53a34f74675992b3759243baad667e80da107d
Isuru Ranawaka <irjanith@gmail.com>
2023-09-06 08:23:03 -0400

    checking communication

--- cpp/algo/algo.hpp
+++ cpp/algo/algo.hpp
@@ -189,9 +189,14 @@ public:
           t = start_clock();
         }
 
-//        this->calc_t_dist_grad_rowptr(csr_block, prevCoordinates, lr, j,
-//                                      batch_size, considering_batch_size, false,
-//                                      true, cache_misses_ptr.get());
+        if (i == iterations / 2) {
+          size_t mem = get_memory_usage();
+          add_memory(mem, "Memory usage");
+        }
+
+        this->calc_t_dist_grad_rowptr(csr_block, prevCoordinates, lr, j,
+                                      batch_size, considering_batch_size, false,
+                                      true, cache_misses_ptr.get());
 
         if (alpha > 0 and alpha < 1.0) {
           MPI_Barrier(MPI_COMM_WORLD);
@@ -216,10 +221,10 @@ public:
           t = start_clock();
         }
 
-//        this->calc_t_dist_replus_rowptr(prevCoordinates, random_number_vec, lr,
-//                                        j, batch_size, considering_batch_size);
+        this->calc_t_dist_replus_rowptr(prevCoordinates, random_number_vec, lr,
+                                        j, batch_size, considering_batch_size);
 
-//        this->update_data_matrix_rowptr(prevCoordinates, j, batch_size);
+        this->update_data_matrix_rowptr(prevCoordinates, j, batch_size);
 
         if (this->grid->world_size > 1 and
             !(i == iterations - 1 and j == batches - 1) and alpha > 0) {
