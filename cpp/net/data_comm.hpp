5869be2b706b2406c948d89bb219e93af8bde542
Isuru Ranawaka <irjanith@gmail.com>
2023-09-08 12:58:18 -0400

    checking communication

--- cpp/net/data_comm.hpp
+++ cpp/net/data_comm.hpp
@@ -455,6 +455,8 @@ public:
               ->insert_cache(i, t.col, batch_id, iteration, t.value,temp);
       }
     }
+    receivebuf->clear();
+    receivebuf->sendbuf->shrink_to_fit();
   }
 
   void cross_validate_batch_from_metadata(int batch_id) {
