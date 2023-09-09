ec7a0b6b470a54ccfc1d32d985e17d7f5d117ecb
Isuru Ranawaka <irjanith@gmail.com>
2023-09-09 16:09:03 -0400

    checking communication

--- tests/data_structure_tests.cpp
+++ tests/data_structure_tests.cpp
@@ -116,7 +116,7 @@ int main(int argc, char **argv) {
                                                                      shared_sparseMat_receiver.get(),
                                                                      shared_sparseMat_sender.get(),
                                                                      dense_mat.get(),
-                                                                     grid.get(),0,0.75,
+                                                                     grid.get(),0,1,
                                                                      5,
                                                                      -5));
 
