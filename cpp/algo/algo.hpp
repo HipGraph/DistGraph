98f58e062aefcb88de8be06ea433e8808b8003fd
Isuru Ranawaka <irjanith@gmail.com>
2023-09-15 21:45:31 -0400

    remove openmp

--- cpp/algo/algo.hpp
+++ cpp/algo/algo.hpp
@@ -226,7 +226,7 @@ public:
             t = start_clock();
             //
             if (!(i == 0 and j == 0)) {
-              data_comm_cache[j - 1].get()->populate_cache(update_ptr.get(), mpi_requests[0], false, i-1, j-1, false);
+              data_comm_cache[j - 1].get()->populate_cache(update_ptr.get(), mpi_requests[0], false, (j==0)?i-1:i, j-1, false);
             }
 
             stop_clock_and_add(t, "Communication Time");
