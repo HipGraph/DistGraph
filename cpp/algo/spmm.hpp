1f346309358fc1a8e6344932f53c8062f624d286
Isuru Ranawaka <irjanith@gmail.com>
2023-09-17 15:04:08 -0400

    remove openmp

--- cpp/algo/spmm.hpp
+++ cpp/algo/spmm.hpp
@@ -317,7 +317,7 @@ public:
             MPI_Barrier(MPI_COMM_WORLD);
             stop_clock_and_add(t, "Computation Time");
             t = start_clock();
-            this->data_comm_cache[0].get()->data_comm_cache_misses_update[k].get()->transfer_data(cache_misses_col_ptr.get(), i, 0, k, end_process);
+//            this->data_comm_cache[0].get()->data_comm_cache_misses_update[k].get()->transfer_data(cache_misses_col_ptr.get(), i, 0, k, end_process);
             stop_clock_and_add(t, "Communication Time");
             t = start_clock();
 //            this->calc_t_dist_grad_for_cache_misses(cache_misses_ptr.get(), prevCoordinates, i, 0, batch_size, lr,k, end_process);
@@ -461,11 +461,11 @@ public:
                 MPI_Barrier(MPI_COMM_WORLD);
                 stop_clock_and_add(t, "Computation Time");
                 t = start_clock();
-//                this->data_comm_cache[j].get()->data_comm_cache_misses_update[k].get()->transfer_data(cache_misses_col_ptr.get(), i, j, k,end_process);
+                this->data_comm_cache[j].get()->data_comm_cache_misses_update[k].get()->transfer_data(cache_misses_col_ptr.get(), i, j, k,end_process);
                 stop_clock_and_add(t, "Communication Time");
                 t = start_clock();
-//                this->calc_t_dist_grad_for_cache_misses(cache_misses_ptr.get(), prevCoordinates, i, j, batch_size,lr,
-//                                                        k, end_process);
+                this->calc_t_dist_grad_for_cache_misses(cache_misses_ptr.get(), prevCoordinates, i, j, batch_size,lr,
+                                                        k, end_process);
               }
             }
           }
