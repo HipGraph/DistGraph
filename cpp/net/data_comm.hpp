e270e30811294e5174e07193548a90684734d16e
Isuru Ranawaka <irjanith@gmail.com>
2023-09-21 18:30:29 -0400

    fixing issues

--- cpp/net/data_comm.hpp
+++ cpp/net/data_comm.hpp
@@ -503,7 +503,7 @@ public:
 };
 
 template <typename SPT, typename DENT, size_t embedding_dim>
-std::unordered_map<uint64_t, std::vector<BatchToProcMap>> DataComm<SPT, DENT, embedding_dim>::send_indices_to_proc_map;
+std::unordered_map<uint64_t, std::vector<vector<int>>> DataComm<SPT, DENT, embedding_dim>::send_indices_to_proc_map;
 
 
 } // namespace distblas::net
