a4c45b4a542cf0473b826319d3fcff25f8b82948
Isuru Ranawaka <irjanith@gmail.com>
2023-09-20 21:15:32 -0400

    fixing issues

--- cpp/net/data_comm.hpp
+++ cpp/net/data_comm.hpp
@@ -107,37 +107,37 @@ public:
     // calculating receiving data cols
     this->sp_local_receiver->fill_col_ids(batch_id, receive_col_ids_list,alpha);
 
-    // calculating sending data cols
-    this->sp_local_sender->fill_col_ids(batch_id, send_col_ids_list, alpha);
-
-    // This needs to be changed
-    for (int i = 0; i < grid->world_size; i++) {
-
-      std::unordered_set<uint64_t> unique_set_receiv(
-          receive_col_ids_list[i].begin(), receive_col_ids_list[i].end());
-
-      std::unordered_set<uint64_t> unique_set_send(send_col_ids_list[i].begin(),
-                                                   send_col_ids_list[i].end());
-
-
-      receive_col_ids_list[i] =
-          vector<uint64_t>(unique_set_receiv.begin(), unique_set_receiv.end());
-
-      receivecounts[i] = receive_col_ids_list[i].size();
-
-      send_col_ids_list[i] =
-          vector<uint64_t>(unique_set_send.begin(), unique_set_send.end());
-
-      sendcounts[i] = send_col_ids_list[i].size();
-      total_send_count += sendcounts[i];
-      sdispls[i] = (i > 0) ? sdispls[i - 1] + sendcounts[i - 1] : sdispls[i];
-      rdispls[i] = (i > 0) ? rdispls[i - 1] + receivecounts[i - 1] : rdispls[i];
-
-      for (int j = 0; j < send_col_ids_list[i].size(); j++) {
-        uint64_t local_key = send_col_ids_list[i][j];
-        send_indices_to_proc_map[local_key][i] = 1;
-      }
-    }
+//    // calculating sending data cols
+//    this->sp_local_sender->fill_col_ids(batch_id, send_col_ids_list, alpha);
+//
+//    // This needs to be changed
+//    for (int i = 0; i < grid->world_size; i++) {
+//
+//      std::unordered_set<uint64_t> unique_set_receiv(
+//          receive_col_ids_list[i].begin(), receive_col_ids_list[i].end());
+//
+//      std::unordered_set<uint64_t> unique_set_send(send_col_ids_list[i].begin(),
+//                                                   send_col_ids_list[i].end());
+//
+//
+//      receive_col_ids_list[i] =
+//          vector<uint64_t>(unique_set_receiv.begin(), unique_set_receiv.end());
+//
+//      receivecounts[i] = receive_col_ids_list[i].size();
+//
+//      send_col_ids_list[i] =
+//          vector<uint64_t>(unique_set_send.begin(), unique_set_send.end());
+//
+//      sendcounts[i] = send_col_ids_list[i].size();
+//      total_send_count += sendcounts[i];
+//      sdispls[i] = (i > 0) ? sdispls[i - 1] + sendcounts[i - 1] : sdispls[i];
+//      rdispls[i] = (i > 0) ? rdispls[i - 1] + receivecounts[i - 1] : rdispls[i];
+//
+//      for (int j = 0; j < send_col_ids_list[i].size(); j++) {
+//        uint64_t local_key = send_col_ids_list[i][j];
+//        send_indices_to_proc_map[local_key][i] = 1;
+//      }
+//    }
   }
 
   void transfer_data(std::vector<DataTuple<DENT, embedding_dim>> *receivebuf,
