831e96164fe3ac285fa2f4324f60dd06da5c89bd
Isuru Ranawaka <irjanith@gmail.com>
2023-10-25 11:25:57 -0400

    algo update

--- tests/data_structure_tests.cpp
+++ tests/data_structure_tests.cpp
@@ -193,18 +193,18 @@ int main(int argc, char **argv) {
 //  //
 //  //  cout << " rank " << rank << " training completed  " << endl;
 //  ofstream fout;
-//  fout.open("perf_output", std::ios_base::app);
+  fout.open("perf_output", std::ios_base::app);
 //
   json j_obj;
   j_obj["alpha"] = alpha;
   j_obj["beta"] = beta;
   j_obj["algo"] = "Embedding";
-  j_obj["p"] = world_size;
+//  j_obj["p"] = world_size;
   j_obj["data_set"] = data_set_name;
   j_obj["perf_stats"] = json_perf_statistics();
-  if (rank == 0) {
+//  if (rank == 0) {
     fout << j_obj.dump(4) << "," << endl;
-  }
+//  }
 //
 //  fout.close();
 //  //
