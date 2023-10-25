00964105d409e4a48a8595a670306d21c6800400
Isuru Ranawaka <irjanith@gmail.com>
2023-10-25 08:05:44 -0400

    algo update

--- cpp/algo/algo.hpp
+++ cpp/algo/algo.hpp
@@ -218,8 +218,8 @@ public:
                                         true, true, 0, 0, false);
         }
 
-//        this->calc_t_dist_replus_rowptr(prevCoordinates, random_number_vec, lr,
-//                                        j, batch_size, considering_batch_size);
+        this->calc_t_dist_replus_rowptr(prevCoordinates, random_number_vec, lr,
+                                        j, batch_size, considering_batch_size);
 
         this->update_data_matrix_rowptr(prevCoordinates, j, batch_size);
         for (int k = 0; k < batch_size; k += 1) {
@@ -453,7 +453,7 @@ public:
     if (csr_block->handler != nullptr) {
       CSRHandle *csr_handle = csr_block->handler.get();
 
-      #pragma omp parallel for schedule(static)
+//      #pragma omp parallel for schedule(static)
       for (uint64_t i = dst_start_index; i <= dst_end_index; i++) {
 
         uint64_t local_dst = i - (this->grid)->global_rank *
