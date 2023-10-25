df544a844db6f10804045a64ea804dcc14dc808d
Isuru Ranawaka <irjanith@gmail.com>
2023-10-25 18:03:35 -0400

    algo update

--- cpp/algo/algo.hpp
+++ cpp/algo/algo.hpp
@@ -225,7 +225,7 @@ public:
           this->calc_t_dist_replus_rowptr(prevCoordinates, random_number_vec, lr,
                                           j, batch_size, considering_batch_size);
 //          dense_local->invalidate_cache(i, j, true);
-//          cout << " rank " << grid->global_rank << " iteration " << i << "repulsive calculation completed "<<"batch "<<j<< endl;
+          cout << " rank " << grid->global_rank << " iteration " << i << "repulsive calculation completed "<<"batch "<<j<< endl;
 
 
           //  pull model code
@@ -246,14 +246,14 @@ public:
                 this->calc_t_dist_grad_rowptr(
                     csr_block, prevCoordinates, lr, j, batch_size,
                     considering_batch_size, true, true, 0, 0, false);
-//                cout << " rank " << grid->global_rank << " iteration " << i << "local computation completed "<<"batch "<<j<< endl;
+                cout << " rank " << grid->global_rank << " iteration " << i << "local computation completed "<<"batch "<<j<< endl;
 
               } else if (k > 1) {
                 int prev_end_process = get_end_proc(prev_start, beta, grid->world_size);
                 this->calc_t_dist_grad_rowptr(csr_block, prevCoordinates, lr, j, batch_size,
                     considering_batch_size, false, true, prev_start,
                     prev_end_process, true);
-//                cout << " rank " << grid->global_rank << " iteration " << i << "remote computation completed "<<"batch "<<j<< endl;
+                cout << " rank " << grid->global_rank << " iteration " << i << "remote computation completed "<<"batch "<<j<< endl;
 //                dense_local->invalidate_cache(i, j, true);
               }
 
@@ -262,15 +262,14 @@ public:
               prev_start = k;
               update_ptr.get()->clear();
             }
-            int prev_end_process =
-                get_end_proc(prev_start, beta, grid->world_size);
+            int prev_end_process = get_end_proc(prev_start, beta, grid->world_size);
 
             // updating last remote fetched data vectors
             this->calc_t_dist_grad_rowptr(csr_block, prevCoordinates, lr, j,
                                           batch_size, considering_batch_size,
                                           false, true, prev_start,
                                           prev_end_process, true);
-//            cout << " rank " << grid->global_rank << " iteration " << i << " final remote computation completed "<<"batch "<<j<< endl;
+            cout << " rank " << grid->global_rank << " iteration " << i << " final remote computation completed "<<"batch "<<j<< endl;
 
 //            dense_local->invalidate_cache(i, j, true);
             update_ptr.get()->resize(0);
