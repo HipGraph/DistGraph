a25bf2653615f4df84c71d6aa5828c775bd32345
Isuru Ranawaka <irjanith@gmail.com>
2023-11-02 09:03:23 -0400

    bug fix

--- cpp/algo/algo.hpp
+++ cpp/algo/algo.hpp
@@ -258,9 +258,9 @@ public:
           }
 
           full_comm.get()->transfer_data(random_number_vec, i, j);
-          this->calc_t_dist_replus_rowptr(prevCoordinates, random_number_vec,
-                                          lr, j, batch_size,
-                                          considering_batch_size);
+//          this->calc_t_dist_replus_rowptr(prevCoordinates, random_number_vec,
+//                                          lr, j, batch_size,
+//                                          considering_batch_size);
           //          dense_local->invalidate_cache(i, j, true);
           //          cout << " rank " << grid->global_rank << " iteration " <<
           //          i << "repulsive calculation completed "<<"batch "<<j<<
@@ -297,10 +297,10 @@ public:
               } else if (k > 1) {
                 int prev_end_process =
                     get_end_proc(prev_start, beta, grid->world_size);
-                this->calc_t_dist_grad_rowptr(
-                    csr_block, prevCoordinates, lr, j, batch_size,
-                    considering_batch_size, false, true, prev_start,
-                    prev_end_process, true);
+//                this->calc_t_dist_grad_rowptr(
+//                    csr_block, prevCoordinates, lr, j, batch_size,
+//                    considering_batch_size, false, true, prev_start,
+//                    prev_end_process, true);
                 //                cout << " rank " << grid->global_rank << "
                 //                iteration " << i << "remote computation
                 //                completed "<<"batch "<<j<< endl;
