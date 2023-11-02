7a2e503c3ef3d2b0b9039005a50d956dbfb5f0e5
Isuru Ranawaka <irjanith@gmail.com>
2023-11-02 10:58:28 -0400

    bug fix

--- cpp/algo/algo.hpp
+++ cpp/algo/algo.hpp
@@ -319,7 +319,7 @@ public:
             // updating last remote fetched data vectors
             this->calc_t_dist_grad_rowptr(csr_block_row, prevCoordinates, lr, j,
                                           batch_size, considering_batch_size,
-                                          false, true, prev_start,
+                                          false, false, prev_start,
                                           prev_end_process, true);
             //            cout << " rank " << grid->global_rank << " iteration "
             //            << i << " final remote computation completed "<<"batch
