53b02d6138cf0d5ade4c934e70c7fac404ae3918
Isuru Ranawaka <irjanith@gmail.com>
2023-11-02 12:50:42 -0400

    bug fix

--- cpp/algo/algo.hpp
+++ cpp/algo/algo.hpp
@@ -287,9 +287,8 @@ public:
 
               if (k == 1) {
                 // local computation
-//                this->calc_t_dist_grad_rowptr(
-//                    csr_block_row, prevCoordinates, lr, j, batch_size,
-//                    considering_batch_size, true, false, 0, 0, true);
+                this->calc_t_dist_grad_rowptr(csr_block_row, prevCoordinates, lr, j, batch_size,
+                    considering_batch_size, true, false, 0, 0, true);
                 //                cout << " rank " << grid->global_rank << "
                 //                iteration " << i << "local computation
                 //                completed "<<"batch "<<j<< endl;
@@ -307,8 +306,7 @@ public:
                 //                dense_local->invalidate_cache(i, j, true);
               }
 
-              //              data_comm_cache[j].get()->populate_cache(update_ptr.get(),
-              //              request_batch_update_cyclic, false, i, j,false);
+              data_comm_cache[j].get()->populate_cache(update_ptr.get(),request_batch_update_cyclic, false, i, j,true);
 
               prev_start = k;
               update_ptr.get()->clear();
