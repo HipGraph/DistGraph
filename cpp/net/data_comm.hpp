89b3f0b8eae45c6d9b8694140ee1efdd596b2fc9
Isuru Ranawaka <irjanith@gmail.com>
2023-11-04 11:41:55 -0400

    bug fix

--- cpp/net/data_comm.hpp
+++ cpp/net/data_comm.hpp
@@ -212,9 +212,10 @@ public:
                      sdispls_cyclic.data(), DENSETUPLE, (*receivebuf).data(),
                      receive_counts_cyclic.data(), rdispls_cyclic.data(),
                      DENSETUPLE, MPI_COMM_WORLD, req);
+
       std::this_thread::sleep_for(std::chrono::seconds(20));
 
-      this->populate_cache(receivebuf, req, true, iteration, batch_id,temp_cache);
+      this->populate_cache(receivebuf, req, false, iteration, batch_id,temp_cache);
 
       stop_clock_and_add(t, "Communication Time");
     }
