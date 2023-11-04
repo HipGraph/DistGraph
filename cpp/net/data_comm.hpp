c1926389fca4455b787bc7545b2403ab6ca71416
Isuru Ranawaka <irjanith@gmail.com>
2023-11-04 10:14:43 -0400

    bug fix

--- cpp/net/data_comm.hpp
+++ cpp/net/data_comm.hpp
@@ -116,7 +116,7 @@ public:
   }
 
   void transfer_data(std::vector<DataTuple<DENT, embedding_dim>> *receivebuf,
-                     bool synchronous,vector<MPI_Request> &holder, int iteration,
+                     bool synchronous,vector<MPI_Request*> &holder, int iteration,
                      int batch_id, int starting_proc, int end_proc,
                      bool temp_cache) {
 
