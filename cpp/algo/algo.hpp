f3c2ba0f12dcdb2bea7cc748dfe6d231b18e6132
Isuru Ranawaka <irjanith@gmail.com>
2023-11-05 14:17:05 -0500

    bug fix in alpha>0

--- cpp/algo/algo.hpp
+++ cpp/algo/algo.hpp
@@ -301,19 +301,19 @@ public:
               for (int k = 1; k < alpha_proc_length; k += alpha_cyc_len) {
                 MPI_Request request_batch_update;
 
-                this->data_comm_cache[j].get()->transfer_data(sendbuf_ptr.get(), update_ptr.get(), sync,&request_batch_update, i, j, k, (k + alpha_cyc_len), false);
+//                this->data_comm_cache[j].get()->transfer_data(sendbuf_ptr.get(), update_ptr.get(), sync,&request_batch_update, i, j, k, (k + alpha_cyc_len), false);
 
                 if (!sync) {
-                  MPI_Ialltoallv(
-                      (*sendbuf_ptr.get()).data(),
-                      this->data_comm_cache[j].get()->send_counts_cyclic.data(),
-                      this->data_comm_cache[j].get()->sdispls_cyclic.data(),
-                      DENSETUPLE, (*update_ptr.get()).data(),
-                      this->data_comm_cache[j]
-                          .get()
-                          ->receive_counts_cyclic.data(),
-                      this->data_comm_cache[j].get()->rdispls_cyclic.data(),
-                      DENSETUPLE, MPI_COMM_WORLD, &request_batch_update);
+//                  MPI_Ialltoallv(
+//                      (*sendbuf_ptr.get()).data(),
+//                      this->data_comm_cache[j].get()->send_counts_cyclic.data(),
+//                      this->data_comm_cache[j].get()->sdispls_cyclic.data(),
+//                      DENSETUPLE, (*update_ptr.get()).data(),
+//                      this->data_comm_cache[j]
+//                          .get()
+//                          ->receive_counts_cyclic.data(),
+//                      this->data_comm_cache[j].get()->rdispls_cyclic.data(),
+//                      DENSETUPLE, MPI_COMM_WORLD, &request_batch_update);
                 }
 
                 if (k == 1) {
@@ -329,9 +329,9 @@ public:
                 }
 
                 if (!sync) {
-                  data_comm_cache[j].get()->populate_cache(
-                      sendbuf_ptr.get(), update_ptr.get(),
-                      &request_batch_update, sync, i, j, false);
+//                  data_comm_cache[j].get()->populate_cache(
+//                      sendbuf_ptr.get(), update_ptr.get(),
+//                      &request_batch_update, sync, i, j, false);
                 }
 
                 prev_start_proc = k;
