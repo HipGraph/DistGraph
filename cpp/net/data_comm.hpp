081d29381b5a8a39b25925c9b6d6a7ec03d86470
Isuru Ranawaka <irjanith@gmail.com>
2023-11-05 10:04:36 -0500

    bug fix

--- cpp/net/data_comm.hpp
+++ cpp/net/data_comm.hpp
@@ -210,8 +210,7 @@ public:
                     receive_counts_cyclic.data(), rdispls_cyclic.data(),
                     DENSETUPLE, MPI_COMM_WORLD);
       MPI_Request dumy;
-      this->populate_cache(receivebuf, &dumy, true, iteration, batch_id,
-                           temp_cache);
+      this->populate_cache(sendbuf_cyclic, receivebuf, &dumy, true, iteration, batch_id,temp_cache);
       stop_clock_and_add(t, "Communication Time");
     } else {
       auto t = start_clock();
