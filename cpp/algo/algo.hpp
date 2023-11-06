a08e34faf709a378370cc025f0da0627fafea28e
Isuru Ranawaka <irjanith@gmail.com>
2023-11-05 19:49:47 -0500

    bug fix in alpha>0

--- cpp/algo/algo.hpp
+++ cpp/algo/algo.hpp
@@ -396,8 +396,10 @@ public:
         }
         total_memory += get_memory_usage();
       }
-      dense_local->print_cache(i);
-      dense_local->print_matrix_rowptr(i);
+      if (i=iterations-1) {
+        dense_local->print_cache(i);
+        dense_local->print_matrix_rowptr(i);
+      }
     }
     total_memory = total_memory / (iterations * batches);
     add_memory(total_memory, "Memory usage");
