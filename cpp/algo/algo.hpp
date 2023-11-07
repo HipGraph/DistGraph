7ae82af5c8151f20789c6986c3a228bef1a39670
Isuru Ranawaka <irjanith@gmail.com>
2023-11-07 13:58:01 -0500

    bug fix in alpha>0

--- cpp/algo/algo.hpp
+++ cpp/algo/algo.hpp
@@ -516,7 +516,7 @@ public:
       CSRHandle *csr_handle = csr_block->handler.get();
 
 
-#pragma omp parallel for num_threads(8) schedule(static) // enable for full batch training or // batch size larger than 1000000
+#pragma omp parallel for schedule(static) // enable for full batch training or // batch size larger than 1000000
       for (uint64_t i = source_start_index; i <= source_end_index; i++) {
 
         uint64_t index = i - batch_id * batch_size;
@@ -579,7 +579,7 @@ public:
 
     int row_base_index = batch_id * batch_size;
 
-//#pragma omp parallel for schedule(static)
+#pragma omp parallel for schedule(static)
     for (int i = 0; i < block_size; i++) {
       uint64_t row_id = static_cast<uint64_t>(i + row_base_index);
       DENT forceDiff[embedding_dim];
@@ -644,7 +644,7 @@ public:
     int end_row = std::min((batch_id + 1) * batch_size,
                            ((this->sp_local_receiver)->proc_row_width));
 
-//#pragma omp parallel for schedule(static)
+#pragma omp parallel for schedule(static)
     for (int i = 0; i < (end_row - row_base_index); i++) {
       for (int d = 0; d < embedding_dim; d++) {
         (this->dense_local)
