3275d6b8cca5fb243a11cb1ab8aa30f6724b7f1d
Isuru Ranawaka <irjanith@gmail.com>
2023-11-08 11:29:46 -0500

    bug fix in alpha>0

--- cpp/dist_embed.cpp
+++ cpp/dist_embed.cpp
@@ -203,7 +203,7 @@ int main(int argc, char **argv) {
 //
   fout.close();
   //
-  reader->parallel_write("embedding.txt",dense_mat.get()->nCoordinates,localARows, dimension);
+  reader->parallel_write(output_file+"/embedding.txt",dense_mat.get()->nCoordinates,localARows, dimension);
 
 
   MPI_Finalize();
