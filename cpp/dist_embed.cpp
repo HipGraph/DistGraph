20d49b541ef28fa4816881a014e521f6b16bb6af
Isuru Ranawaka <irjanith@gmail.com>
2023-12-04 15:53:53 -0500

    fix batch_size issue

--- cpp/dist_embed.cpp
+++ cpp/dist_embed.cpp
@@ -44,7 +44,7 @@ int main(int argc, char **argv) {
   bool col_major = false;
   bool sync_comm  = false;
 
-  bool full_batch_training = true;
+  bool fix_batch_training = true;
 
   for (int p = 0; p < argc; p++) {
     if (strcmp(argv[p], "-input") == 0) {
@@ -71,9 +71,9 @@ int main(int argc, char **argv) {
     } else if (strcmp(argv[p], "-sync_comm") == 0) {
       int val = atoi(argv[p + 1]);
       sync_comm = (val != 0) ? true : false;
-    } else if (strcmp(argv[p], "-full_batch_training") == 0) {
+    } else if (strcmp(argv[p], "-fix_batch_training") == 0) {
       int full_batch_tra = atoi(argv[p + 1]);
-      full_batch_training = full_batch_tra == 1 ? true : false;
+      fix_batch_training = full_batch_tra == 1 ? true : false;
     }
   }
 
@@ -84,7 +84,7 @@ int main(int argc, char **argv) {
   int world_size;
   MPI_Comm_rank(MPI_COMM_WORLD, &rank);
   MPI_Comm_size(MPI_COMM_WORLD, &world_size);
-  if (full_batch_training) {
+  if (fix_batch_training) {
     batch_size = batch_size / world_size;
   }
 
