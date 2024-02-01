36cc5fea950fc31d7eb8c2a41a19b07958dd8bb7
Isuru Ranawaka <irjanith@gmail.com>
2024-02-01 11:48:16 -0500

    fixing comm timing issue

--- cpp/net/data_comm.hpp
+++ cpp/net/data_comm.hpp
@@ -322,8 +322,7 @@ public:
     }
 
 
-
-    t = start_clock();
+   auto t = start_clock();
     MPI_Alltoall(send_counts_cyclic.data(), 1,MPI_INT,receive_counts_cyclic.data(),1,MPI_INT,grid->col_world);
     stop_clock_and_add(t, "Communication Time");
 
