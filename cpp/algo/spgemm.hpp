a364134396b6006707e2014201e5446e97248170
Isuru Ranawaka <irjanith@gmail.com>
2024-01-31 23:04:17 -0500

    fixing comm timing issue

--- cpp/algo/spgemm.hpp
+++ cpp/algo/spgemm.hpp
@@ -169,7 +169,7 @@ public:
         auto t = start_clock();
         data_comm->transfer_sparse_data(sendbuf, receivebuf,  iteration,
                                         batch, k, end_process);
-        stop_clock_and_add(t, "transfer data");
+        stop_clock_and_add(t, "Transfer Data");
       }
       if (k == comm_initial_start) {
         // local computation
@@ -178,7 +178,7 @@ public:
             csr_block, prevCoordinates, lr, batch, batch_size,
             considering_batch_size, local_execution,
             first_execution_proc, prev_start);
-        stop_clock_and_add(t, "compute local");
+        stop_clock_and_add(t, "Compute  Local");
       } else if (k > comm_initial_start) {
         int prev_end_process = get_end_proc(prev_start, beta, grid->col_world_size);
 
@@ -195,7 +195,7 @@ public:
     this->calc_t_dist_grad_rowptr(csr_block, prevCoordinates, lr, batch,
                                   batch_size, considering_batch_size,
                                   false,prev_start, prev_end_process);
-    stop_clock_and_add(t, "compute remote");
+    stop_clock_and_add(t, "Compute  Remote");
     // dense_local->invalidate_cache(i, j, true);
   }
 
--- cpp/core/common.cpp
+++ cpp/core/common.cpp
@@ -10,7 +10,7 @@ MPI_Datatype distblas::core::DENSETUPLE;
 MPI_Datatype distblas::core::SPARSETUPLE;
 
 vector<string> distblas::core::perf_counter_keys = {
-    "Computation Time", "Communication Time", "Memory usage", "Data transfers","Total Time","transfer data","compute  local","compute  remote"};
+    "Computation Time", "Communication Time", "Memory usage", "Data transfers","Total Time","Transfer Data","Compute  Local","Compute  Remote"};
 
 map<string, int> distblas::core::call_count;
 map<string, double> distblas::core::total_time;
