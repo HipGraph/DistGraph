b9d80f8bed623044ca9c42f9df9441e6adff71ec
Isuru Ranawaka <irjanith@gmail.com>
2024-02-03 15:53:32 -0500

    fixing comm timing issue

--- cpp/dist_embed.cpp
+++ cpp/dist_embed.cpp
@@ -291,7 +291,7 @@ int main(int argc, char **argv) {
   //
 // reader->parallel_write(output_file+"/embedding.txt",dense_mat.get()->nCoordinates,localARows, dimension, grid.get(),shared_sparseMat.get());
  if(spgemm & save_results){
-   reader->parallel_write(output_file+"/sparse_local.txt",sparse_coo,grid.get(), localARows);
+   reader->parallel_write(output_file+"/sparse_local.txt",sparse_coo,grid.get(), localARows,shared_sparseMat.get()->gRows);
  }
 
   MPI_Finalize();
