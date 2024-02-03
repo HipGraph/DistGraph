4b3b4275a0c0d65f6f555f7757c2540c263035ab
Isuru Ranawaka <irjanith@gmail.com>
2024-02-03 10:47:37 -0500

    fixing comm timing issue

--- cpp/io/parrallel_IO.hpp
+++ cpp/io/parrallel_IO.hpp
@@ -174,8 +174,9 @@ public:
 
     for (uint64_t i=0;i<sparse_coo.size();i++) {
           Tuple<T> t  = sparse_coo[i];
+          uint64_t col = static_cast<uint64_t>(t.col);
 //          total_size += snprintf(nullptr, 0, "%ld", static_cast<long>(t.row + 1 + grid->rank_in_col * rows));
-          total_size += snprintf(nullptr, 0, "%ld", static_cast<long>(t.col + 1));
+          total_size += snprintf(nullptr, 0, "%lu", col);
           total_size += snprintf(nullptr, 0, " %.5f", t.value);
           total_size += snprintf(nullptr, 0, "\n");
     }
@@ -193,7 +194,8 @@ public:
     for (uint64_t i = 0; i < sparse_coo.size(); ++i){
       Tuple<T> t  = sparse_coo[i];
 //      current_position += snprintf(current_position, total_size, "%ld", static_cast<long>(t.row + 1 + grid->rank_in_col * rows));
-      current_position += snprintf(current_position, total_size, "%ld", static_cast<long>(t.col + 1));
+      uint64_t col = static_cast<uint64_t>(t.col);
+      current_position += snprintf(current_position, total_size, "%lu", col);
       current_position += snprintf(current_position, total_size, " %.5f", t.value);
       current_position += snprintf(current_position, total_size, "\n");
     }
