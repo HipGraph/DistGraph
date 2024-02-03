38f66149fb801112e756e143bab25d312b84a84f
Isuru Ranawaka <irjanith@gmail.com>
2024-02-03 13:04:05 -0500

    fixing comm timing issue

--- cpp/io/parrallel_IO.hpp
+++ cpp/io/parrallel_IO.hpp
@@ -177,7 +177,7 @@ public:
           int col = static_cast<int>(t.col+1);
           uint64_t row = static_cast<uint64_t>(t.row+1+grid->rank_in_col*rows);
           total_size += snprintf(nullptr, 0, "%lu", row);
-//          total_size += snprintf(nullptr, 0, "%lu", col);
+          total_size += snprintf(nullptr, 0, "%lu", col);
           total_size += snprintf(nullptr, 0, " %.5f", t.value);
           total_size += snprintf(nullptr, 0, "\n");
     }
@@ -199,7 +199,7 @@ public:
       uint64_t row = static_cast<uint64_t>(t.row+1 + grid->rank_in_col*rows);
 
       current_position += snprintf(current_position, total_size, "%lu", row);
-//      current_position += snprintf(current_position, total_size, "%lu", col);
+      current_position += snprintf(current_position, total_size, "%lu", col);
       current_position += snprintf(current_position, total_size, " %.5f", t.value);
       current_position += snprintf(current_position, total_size, "\n");
     }
