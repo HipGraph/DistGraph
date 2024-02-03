60a3a729ef4fa1da0cb0edbfca6dfca613947580
Isuru Ranawaka <irjanith@gmail.com>
2024-02-03 17:59:02 -0500

    fixing comm timing issue

--- cpp/net/data_comm.hpp
+++ cpp/net/data_comm.hpp
@@ -265,6 +265,7 @@ public:
         auto col_id = pair.first;
         cout<<" rank "<<grid->rank_in_col<<" requesting global key "<<col_id<<endl;
         CSRHandle sparse_tuple =  (this->sparse_local)->fetch_local_data(col_id);
+        cout<<" rank "<<grid->rank_in_col<<" requesting global key "<<col_id<<" succeeded "<<endl;
         for (int i = 0; i < sending_procs.size(); i++) {
           if (pair.second.count(sending_procs[i]) > 0) {
 
