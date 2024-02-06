2fdedd32edda4cd84184b2a4d86d4915b6d90798
Isuru Ranawaka <irjanith@gmail.com>
2024-02-06 14:44:03 -0500

    fixing comm timing issue

--- cpp/algo/spgemm.hpp
+++ cpp/algo/spgemm.hpp
@@ -297,9 +297,9 @@ public:
             if (!fetch_from_cache) {
               for (auto k = handle->rowStart[local_dst]; k < handle->rowStart[local_dst + 1]; k++) {
                 auto d = handle->col_idx[k];
-                if (d>=12800){
-                  cout<<" rank "<<grid->rank_in_col<<" larger value "<<d<<endl;
-                }
+                if ((index * embedding_dim + d)>=(sparse_local_output->proc_row_width*sparse_local_output->proc_col_width)){
+                  cout<<" rank "<<grid->rank_in_col<<" wrong index "<<index<<" max value "<<(index * embedding_dim + d)<<" d "<<d<<" expected "<<(sparse_local_output->proc_row_width*sparse_local_output->proc_col_width)<<endl
+
                 prevCoordinates[index * embedding_dim + d] += lr *handle->values[k];
               }
             }else{
