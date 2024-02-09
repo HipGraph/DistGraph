0c5c3af09e703f9ec10544f7350bbae2e60ddd89
Isuru Ranawaka <irjanith@gmail.com>
2024-02-09 18:35:58 -0500

    checking symbolic calc ideas

--- cpp/algo/spgemm.hpp
+++ cpp/algo/spgemm.hpp
@@ -298,7 +298,7 @@ public:
                        (*(sparse_local_output->sparse_data_collector))[index][hash].second =   value;
                        break;
                      }else {
-                       cout<<"hash "<< hash<< " key "<<(*(sparse_local_output->sparse_data_collector))[index][hash].first<<" d "<<d<<" "<<endl;
+                       cout<<" index "<<index<<"hash "<< hash<< " key "<<(*(sparse_local_output->sparse_data_collector))[index][hash].first<<" d "<<d<<" "<<endl;
                        hash = (hash+1)& (ht_size-1);
                      }
                    }
