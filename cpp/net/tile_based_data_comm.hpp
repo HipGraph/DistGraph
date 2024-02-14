d54566a71d1bdd974d5028eb4eae0acbab07d261
Isuru Ranawaka <irjanith@gmail.com>
2024-02-14 12:45:43 -0500

    Adding tile data comm class

--- cpp/net/tile_based_data_comm.hpp
+++ cpp/net/tile_based_data_comm.hpp
@@ -22,7 +22,12 @@ public:
   ~TileDataComm() {}
 
   void onboard_data() override {
-
+    if (alpha==0) {
+      // This represents the case for pulling
+      sp_local_receiver->find_col_ids(batch_id, 0, grid->col_world_size, receive_col_ids_list,receive_indices_to_proc_map, 0);
+      // calculating sending data cols
+      sp_local_sender->find_col_ids(batch_id,0,grid->col_world_size, send_col_ids_list,send_indices_to_proc_map, 0);
+    }
   }
 };
 
