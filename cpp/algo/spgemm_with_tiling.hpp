76cbfd16e53120d1a35dabd79ba966a7d8f13327
Isuru Ranawaka <irjanith@gmail.com>
2024-02-14 18:01:00 -0500

    spgemm with tiling

--- cpp/algo/spgemm_with_tiling.hpp
+++ cpp/algo/spgemm_with_tiling.hpp
@@ -75,7 +75,7 @@ public:
     // fetch initial embeddings
     auto main_comm = unique_ptr<TileDataComm<INDEX_TYPE, VALUE_TYPE, embedding_dim>>(
         new TileDataComm<INDEX_TYPE, VALUE_TYPE, embedding_dim>(
-            sp_local_receiver, sp_local_sender, sparse_local, grid, -1, alpha,batches,0.5));
+            sp_local_receiver, sp_local_sender, sparse_local, grid,  alpha,batches,0.5));
     main_comm.get()->onboard_data();
 
 
