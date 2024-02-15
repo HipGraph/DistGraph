6416d12da6a880fbfc84030f76e7351ce421a0e7
Isuru Ranawaka <irjanith@gmail.com>
2024-02-15 17:07:26 -0500

    Introducing tile tuple

--- cpp/net/tile_based_data_comm.hpp
+++ cpp/net/tile_based_data_comm.hpp
@@ -108,11 +108,11 @@ public:
         // calculating sending data cols
         this->sp_local_sender->find_col_ids_with_tiling(
             i, 0, this->grid->col_world_size,
-            sender_proc_tile_map.get(), this->send_indices_to_proc_map, 0);
+            sender_proc_tile_map.get(), this->send_indices_to_proc_map, 0,"+",this->sparse_local);
       }
       // This represents the case for pulling
       this->sparse_local->get_transferrable_datacount(sender_proc_tile_map.get(),total_batches,true, false);
-      this->sp_local_sender->get_transferrable_datacount(sender_proc_tile_map.get(),total_batches,true,spgemm,"+",this->sparse_local);
+      this->sp_local_sender->get_transferrable_datacount(sender_proc_tile_map.get(),total_batches,true,spgemm);
 
       int tiles_per_process = SparseTile<INDEX_TYPE,VALUE_TYPE>::get_tiles_per_process_row();
       auto itr = total_batches * this->grid->col_world_size * tiles_per_process;
