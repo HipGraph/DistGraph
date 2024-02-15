a74e9c00170e73a21910ef524404b035dcc84d40
Isuru Ranawaka <irjanith@gmail.com>
2024-02-15 10:58:10 -0500

    transferable data counts

--- cpp/net/tile_based_data_comm.hpp
+++ cpp/net/tile_based_data_comm.hpp
@@ -107,8 +107,8 @@ public:
             sender_proc_tile_map.get(), this->send_indices_to_proc_map, 0);
       }
       // This represents the case for pulling
-      sparse_local->get_transferrable_datacount(sender_proc_tile_map.get(),true);
-      sparse_local->get_transferrable_datacount(receiver_proc_tile_map.get(),false);
+      this->sparse_local->get_transferrable_datacount(sender_proc_tile_map.get(),true);
+      this->sparse_local->get_transferrable_datacount(receiver_proc_tile_map.get(),false);
     }
   }
 };
