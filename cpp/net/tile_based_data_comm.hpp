85166d5b176e653ededc17a30889dd499a023d31
Isuru Ranawaka <irjanith@gmail.com>
2024-02-18 11:44:02 -0500

    remote computations merge step

--- cpp/net/tile_based_data_comm.hpp
+++ cpp/net/tile_based_data_comm.hpp
@@ -202,7 +202,7 @@ public:
           if (t.count <= t.send_merge_count) {
             (*receiver_proc_tile_map)[i][j][k].mode = 0;
           } else {
-            (*sender_proc_tile_map)[i][j][k].mode = 0;
+//            (*sender_proc_tile_map)[i][j][k].mode = 0;
           }
         }
       }
