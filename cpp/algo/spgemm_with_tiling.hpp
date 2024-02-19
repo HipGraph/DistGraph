95ec46f596a40986f5c1f6d181e9f0cdd96853ad
Isuru Ranawaka <irjanith@gmail.com>
2024-02-19 14:38:01 -0500

    remote computations merge step

--- cpp/algo/spgemm_with_tiling.hpp
+++ cpp/algo/spgemm_with_tiling.hpp
@@ -245,16 +245,17 @@ public:
         if (r != grid->rank_in_col) {
           int computing_rank =(grid->rank_in_col >= r)? (grid->rank_in_col - r) % grid->col_world_size: (grid->col_world_size - r + grid->rank_in_col) %grid->col_world_size;
           for (int tile = 0;tile <(*main_com->sender_proc_tile_map)[batch_id][computing_rank].size();tile++) {
-            if ((*main_com->sender_proc_tile_map)[batch_id][computing_rank][tile].mode ==0) {
-              auto source_start_index =  (*main_com->sender_proc_tile_map)[batch_id][computing_rank][tile].row_starting_index;
-              auto source_end_index =  (*main_com->sender_proc_tile_map)[batch_id][computing_rank][tile].row_end_index;
-              auto dst_start_index = (*main_com->sender_proc_tile_map)[batch_id][computing_rank][tile].col_start_index;
-              auto dst_end_index = (*main_com->sender_proc_tile_map)[batch_id][computing_rank][tile].col_end_index;
-              (*main_com->sender_proc_tile_map)[batch_id][computing_rank][tile].initialize_output_DS_if(0);
-//              calc_embedding_row_major(source_start_index, source_end_index,
-//                                       dst_start_index, dst_end_index,
-//                                       csr_block, lr, batch_id, batch_size,
-//                                       block_size, symbolic,&(*main_com->sender_proc_tile_map)[batch_id][computing_rank][tile]);
+            SparseTile<INDEX_TYPE,VALUE_TYPE>& sp_tile = (*main_com->sender_proc_tile_map)[batch_id][computing_rank][tile];
+            if (sp_tile.mode ==0) {
+              auto source_start_index =  sp_tile.row_starting_index;
+              auto source_end_index =  sp_tile.row_end_index;
+              auto dst_start_index = sp_tile.col_start_index;
+              auto dst_end_index = sp_tile.col_end_index;
+              sp_tile.initialize_output_DS_if(0);
+              calc_embedding_row_major(source_start_index, source_end_index,
+                                       dst_start_index, dst_end_index,
+                                       csr_block, lr, batch_id, batch_size,
+                                       block_size, symbolic,sp_tile);
 //              if (itr==0 and !symbolic){
                 add_tiles(1,"Remote Computed Tiles");
 //              }
