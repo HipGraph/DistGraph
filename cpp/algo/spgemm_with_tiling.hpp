ef5687dd03a8703b4fb57af4e3647fab050aefe6
Isuru Ranawaka <irjanith@gmail.com>
2024-02-19 16:26:01 -0500

    remote computations merge step

--- cpp/algo/spgemm_with_tiling.hpp
+++ cpp/algo/spgemm_with_tiling.hpp
@@ -134,9 +134,9 @@ public:
               main_comm.get(), csr_block, batch_size,
               considering_batch_size, lr,  1,
               true, 0, true,false, this->sparse_local_output);
-          this->calc_t_dist_grad_rowptr((this->sp_local_sender)->csr_local_data.get(),  lr, i,j,
-                                        batch_size, considering_batch_size,
-                                        2,  0, this->grid->col_world_size,false,main_comm.get(),this->sparse_local_output);
+//          this->calc_t_dist_grad_rowptr((this->sp_local_sender)->csr_local_data.get(),  lr, i,j,
+//                                        batch_size, considering_batch_size,
+//                                        2,  0, this->grid->col_world_size,false,main_comm.get(),nullptr);
 
         }
         total_memory += get_memory_usage();
