6ba6e949b2a2e4c9c5d9d685ee0656c1849028a5
Isuru Ranawaka <irjanith@gmail.com>
2024-02-21 07:12:50 -0500

    remote computations merge step

--- cpp/net/tile_based_data_comm.hpp
+++ cpp/net/tile_based_data_comm.hpp
@@ -501,41 +501,41 @@ public:
         }
       }
     }
-//    (*sendbuf_cyclic).resize(total_send_count);
-//    for (int i = 0; i < this->grid->col_world_size; i++) {
-//      this->sdispls_cyclic[i] = (i > 0) ? this->sdispls_cyclic[i - 1] +
-//                                              this->send_counts_cyclic[i - 1]
-//                                        : this->sdispls_cyclic[i];
-//      copy((*data_buffer_ptr)[i].begin(), (*data_buffer_ptr)[i].end(),
-//           (*sendbuf_cyclic).begin() + this->sdispls_cyclic[i]);
-//    }
-//    auto t = start_clock();
-//    MPI_Alltoall(this->send_counts_cyclic.data(), 1, MPI_INT,
-//                 this->receive_counts_cyclic.data(), 1, MPI_INT,
-//                 this->grid->col_world);
-//    stop_clock_and_add(t, "Communication Time");
-//
-//    for (int i = 0; i < this->grid->col_world_size; i++) {
-//      this->rdispls_cyclic[i] = (i > 0) ? this->rdispls_cyclic[i - 1] +
-//                                              this->receive_counts_cyclic[i - 1]
-//                                        : this->rdispls_cyclic[i];
-//      total_receive_count += this->receive_counts_cyclic[i];
-//    }
-//
-//    if (total_receive_count > 0) {
-//      receivebuf->resize(total_receive_count);
-//    }
-//
-//    add_datatransfers(total_receive_count, "Data transfers");
-//
-//    t = start_clock();
-//    MPI_Alltoallv((*sendbuf_cyclic).data(), this->send_counts_cyclic.data(),
-//                  this->sdispls_cyclic.data(), SPARSETUPLE,
-//                  (*receivebuf).data(), this->receive_counts_cyclic.data(),
-//                  this->rdispls_cyclic.data(), SPARSETUPLE,
-//                  this->grid->col_world);
-//    this->store_remotely_computed_data(sendbuf_cyclic,receivebuf,iteration,batch_id);
-//    stop_clock_and_add(t, "Communication Time");
+    (*sendbuf_cyclic).resize(total_send_count);
+    for (int i = 0; i < this->grid->col_world_size; i++) {
+      this->sdispls_cyclic[i] = (i > 0) ? this->sdispls_cyclic[i - 1] +
+                                              this->send_counts_cyclic[i - 1]
+                                        : this->sdispls_cyclic[i];
+      copy((*data_buffer_ptr)[i].begin(), (*data_buffer_ptr)[i].end(),
+           (*sendbuf_cyclic).begin() + this->sdispls_cyclic[i]);
+    }
+    auto t = start_clock();
+    MPI_Alltoall(this->send_counts_cyclic.data(), 1, MPI_INT,
+                 this->receive_counts_cyclic.data(), 1, MPI_INT,
+                 this->grid->col_world);
+    stop_clock_and_add(t, "Communication Time");
+
+    for (int i = 0; i < this->grid->col_world_size; i++) {
+      this->rdispls_cyclic[i] = (i > 0) ? this->rdispls_cyclic[i - 1] +
+                                              this->receive_counts_cyclic[i - 1]
+                                        : this->rdispls_cyclic[i];
+      total_receive_count += this->receive_counts_cyclic[i];
+    }
+
+    if (total_receive_count > 0) {
+      receivebuf->resize(total_receive_count);
+    }
+
+    add_datatransfers(total_receive_count, "Data transfers");
+
+    t = start_clock();
+    MPI_Alltoallv((*sendbuf_cyclic).data(), this->send_counts_cyclic.data(),
+                  this->sdispls_cyclic.data(), SPARSETUPLE,
+                  (*receivebuf).data(), this->receive_counts_cyclic.data(),
+                  this->rdispls_cyclic.data(), SPARSETUPLE,
+                  this->grid->col_world);
+    this->store_remotely_computed_data(sendbuf_cyclic,receivebuf,iteration,batch_id);
+    stop_clock_and_add(t, "Communication Time");
   }
 
   inline void store_remotely_computed_data(vector<SpTuple<VALUE_TYPE, sp_tuple_max_dim>> *sendbuf,
