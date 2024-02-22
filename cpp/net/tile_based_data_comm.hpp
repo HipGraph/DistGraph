6b0184e56250d039d537018221aa5db7d27a1789
Isuru Ranawaka <irjanith@gmail.com>
2024-02-22 09:24:07 -0500

    remote computations merge step

--- cpp/net/tile_based_data_comm.hpp
+++ cpp/net/tile_based_data_comm.hpp
@@ -1,9 +1,9 @@
 #pragma once
+#include "../core/common.h"
 #include "../core/sparse_mat_tile.hpp"
 #include "data_comm.hpp"
 #include <math.h>
 #include <memory>
-#include "../core/common.h"
 
 using namespace distblas::core;
 using namespace std;
@@ -14,8 +14,6 @@ template <typename INDEX_TYPE, typename VALUE_TYPE, size_t embedding_dim>
 class TileDataComm : public DataComm<INDEX_TYPE, VALUE_TYPE, embedding_dim> {
 
 private:
-
-
   shared_ptr<
       vector<vector<unordered_map<INDEX_TYPE, unordered_map<int, bool>>>>>
       send_indices_proc_map;
@@ -51,18 +49,20 @@ public:
         tile_width_fraction;
     receiver_proc_tile_map =
         make_shared<vector<vector<vector<SparseTile<INDEX_TYPE, VALUE_TYPE>>>>>(
-            total_batches, vector<vector<SparseTile<INDEX_TYPE, VALUE_TYPE>>>(
-                               grid->col_world_size,
-                               vector<SparseTile<INDEX_TYPE, VALUE_TYPE>>(
-                                   tiles_per_process_row,
-                                   SparseTile<INDEX_TYPE, VALUE_TYPE>(grid,false))));
+            total_batches,
+            vector<vector<SparseTile<INDEX_TYPE, VALUE_TYPE>>>(
+                grid->col_world_size,
+                vector<SparseTile<INDEX_TYPE, VALUE_TYPE>>(
+                    tiles_per_process_row,
+                    SparseTile<INDEX_TYPE, VALUE_TYPE>(grid, false))));
     sender_proc_tile_map =
         make_shared<vector<vector<vector<SparseTile<INDEX_TYPE, VALUE_TYPE>>>>>(
-            total_batches, vector<vector<SparseTile<INDEX_TYPE, VALUE_TYPE>>>(
-                               grid->col_world_size,
-                               vector<SparseTile<INDEX_TYPE, VALUE_TYPE>>(
-                                   tiles_per_process_row,
-                                   SparseTile<INDEX_TYPE, VALUE_TYPE>(grid,spgemm))));
+            total_batches,
+            vector<vector<SparseTile<INDEX_TYPE, VALUE_TYPE>>>(
+                grid->col_world_size,
+                vector<SparseTile<INDEX_TYPE, VALUE_TYPE>>(
+                    tiles_per_process_row,
+                    SparseTile<INDEX_TYPE, VALUE_TYPE>(grid, spgemm))));
 
     auto tiles_per_process =
         SparseTile<INDEX_TYPE, VALUE_TYPE>::get_tiles_per_process_row();
@@ -77,23 +77,25 @@ public:
         vector<unordered_map<INDEX_TYPE, unordered_map<int, bool>>>(
             tiles_per_process));
 
-    auto total_tiles = total_batches* this->grid->col_world_size*tiles_per_process;
+    auto total_tiles =
+        total_batches * this->grid->col_world_size * tiles_per_process;
 
-    add_tiles(total_tiles,"Total Tiles");
+    add_tiles(total_tiles, "Total Tiles");
 
     if (alpha == 0) {
-      #pragma omp parallel for
+#pragma omp parallel for
       for (int i = 0; i < total_batches; i++) {
         INDEX_TYPE row_starting_index_receiver =
             i * sp_local_receiver->batch_size;
         auto row_end_index_receiver =
-            std::min(std::min(((i+1) *sp_local_receiver->batch_size),
+            std::min(std::min(((i + 1) * sp_local_receiver->batch_size),
                               sp_local_receiver->proc_row_width),
                      sp_local_receiver->gRows);
 
         for (int j = 0; j < grid->col_world_size; j++) {
           INDEX_TYPE row_starting_index_sender =
-              i * sp_local_receiver->batch_size +sp_local_receiver->proc_row_width * j;
+              i * sp_local_receiver->batch_size +
+              sp_local_receiver->proc_row_width * j;
           INDEX_TYPE row_end_index_sender = std::min(
               std::min(
                   (row_starting_index_sender + sp_local_receiver->batch_size),
@@ -114,20 +116,29 @@ public:
                 sp_local_receiver->gCols);
 
             INDEX_TYPE col_starting_index_sender = k * tile_width;
-            auto col_end_index_sender =std::min((col_starting_index_sender + tile_width),
-                                  sp_local_receiver->proc_col_width);
+            auto col_end_index_sender =
+                std::min((col_starting_index_sender + tile_width),
+                         sp_local_receiver->proc_col_width);
 
             (*receiver_proc_tile_map)[i][j][k].id = k;
-            (*receiver_proc_tile_map)[i][j][k].row_starting_index = row_starting_index_receiver;
-            (*receiver_proc_tile_map)[i][j][k].row_end_index = row_end_index_receiver;
-            (*receiver_proc_tile_map)[i][j][k].col_start_index = col_starting_index_receiver;
-            (*receiver_proc_tile_map)[i][j][k].col_end_index = col_end_index_receiver;
+            (*receiver_proc_tile_map)[i][j][k].row_starting_index =
+                row_starting_index_receiver;
+            (*receiver_proc_tile_map)[i][j][k].row_end_index =
+                row_end_index_receiver;
+            (*receiver_proc_tile_map)[i][j][k].col_start_index =
+                col_starting_index_receiver;
+            (*receiver_proc_tile_map)[i][j][k].col_end_index =
+                col_end_index_receiver;
 
             (*sender_proc_tile_map)[i][j][k].id = k;
-            (*sender_proc_tile_map)[i][j][k].row_starting_index = row_starting_index_sender;
-            (*sender_proc_tile_map)[i][j][k].row_end_index = row_end_index_sender;
-            (*sender_proc_tile_map)[i][j][k].col_start_index = col_starting_index_sender;
-            (*sender_proc_tile_map)[i][j][k].col_end_index = col_end_index_sender;
+            (*sender_proc_tile_map)[i][j][k].row_starting_index =
+                row_starting_index_sender;
+            (*sender_proc_tile_map)[i][j][k].row_end_index =
+                row_end_index_sender;
+            (*sender_proc_tile_map)[i][j][k].col_start_index =
+                col_starting_index_sender;
+            (*sender_proc_tile_map)[i][j][k].col_end_index =
+                col_end_index_sender;
           }
         }
       }
@@ -164,7 +175,7 @@ public:
       unique_ptr<vector<TileTuple<INDEX_TYPE>>> receive_tile_meta =
           make_unique<vector<TileTuple<INDEX_TYPE>>>(itr);
 
-     #pragma omp parallel for
+#pragma omp parallel for
       for (auto in = 0; in < itr; in++) {
         auto i = in / (this->grid->col_world_size * tiles_per_process);
         auto j = (in / tiles_per_process) % this->grid->col_world_size;
@@ -180,8 +191,8 @@ public:
         t.send_merge_count =
             (*sender_proc_tile_map)[i][j][k].total_receivable_datacount;
         (*send_tile_meta)[index] = t;
-        if (t.count > t.send_merge_count){
-          (*sender_proc_tile_map)[i][j][k].mode=0;
+        if (t.count > t.send_merge_count) {
+          (*sender_proc_tile_map)[i][j][k].mode = 0;
         }
       }
 
@@ -204,8 +215,10 @@ public:
               t.send_merge_count;
           if (t.count <= t.send_merge_count) {
             (*receiver_proc_tile_map)[i][j][k].mode = 0;
-          }else{
-            (*receiver_proc_tile_map)[i][j][k].initialize_dataCache();//initialize data cache to receive remote computed data
+          } else {
+            (*receiver_proc_tile_map)[i][j][k]
+                .initialize_dataCache(); // initialize data cache to receive
+                                         // remote computed data
           }
         }
       }
@@ -253,7 +266,8 @@ public:
         CSRHandle sparse_tuple = (this->sparse_local)->fetch_local_data(col_id);
         for (int i = 0; i < sending_procs.size(); i++) {
           if (pair.second.count(sending_procs[i]) > 0 and
-              (*sender_proc_tile_map)[batch_id][sending_procs[i]][tile].mode ==1) {
+              (*sender_proc_tile_map)[batch_id][sending_procs[i]][tile].mode ==
+                  1) {
             if (this->send_counts_cyclic[sending_procs[i]] == 0) {
               SpTuple<VALUE_TYPE, sp_tuple_max_dim> current;
               current.rows[0] =
@@ -266,7 +280,8 @@ public:
 
             SpTuple<VALUE_TYPE, sp_tuple_max_dim> latest =
                 (*data_buffer_ptr)[sending_procs[i]]
-                                  [this->send_counts_cyclic[sending_procs[i]] -1];
+                                  [this->send_counts_cyclic[sending_procs[i]] -
+                                   1];
             auto row_index_offset = latest.rows[0];
             auto col_index_offset = latest.rows[1];
             if (row_index_offset >= row_max or
@@ -387,7 +402,8 @@ public:
 
     int total_receive_count = 0;
     unique_ptr<vector<vector<SpTuple<VALUE_TYPE, sp_tuple_max_dim>>>>
-        data_buffer_ptr = make_unique<vector<vector<SpTuple<VALUE_TYPE, sp_tuple_max_dim>>>>();
+        data_buffer_ptr = make_unique<
+            vector<vector<SpTuple<VALUE_TYPE, sp_tuple_max_dim>>>>();
     data_buffer_ptr->resize(this->grid->col_world_size);
 
     int total_send_count = 0;
@@ -415,11 +431,19 @@ public:
 
     for (int i = 0; i < sending_procs.size(); i++) {
       for (int tile = start_tile; tile < end_tile; tile++) {
-        if ((*sender_proc_tile_map)[batch_id][sending_procs[i]][tile].mode ==0) {
-          (*sender_proc_tile_map)[batch_id][sending_procs[i]][tile].initialize_CSR_from_sparse_collector();
-          for (INDEX_TYPE index =(*sender_proc_tile_map)[batch_id][sending_procs[i]][tile].row_starting_index;
-               index < (*sender_proc_tile_map)[batch_id][sending_procs[i]][tile].row_end_index;++index) {
-            CSRHandle sparse_tuple =(*sender_proc_tile_map)[batch_id][sending_procs[i]][tile].fetch_remote_data(index);
+        if ((*sender_proc_tile_map)[batch_id][sending_procs[i]][tile].mode ==
+            0) {
+          (*sender_proc_tile_map)[batch_id][sending_procs[i]][tile]
+              .initialize_CSR_from_sparse_collector();
+          for (INDEX_TYPE index =
+                   (*sender_proc_tile_map)[batch_id][sending_procs[i]][tile]
+                       .row_starting_index;
+               index < (*sender_proc_tile_map)[batch_id][sending_procs[i]][tile]
+                           .row_end_index;
+               ++index) {
+            CSRHandle sparse_tuple =
+                (*sender_proc_tile_map)[batch_id][sending_procs[i]][tile]
+                    .fetch_remote_data(index);
 
             if (this->send_counts_cyclic[sending_procs[i]] == 0) {
               SpTuple<VALUE_TYPE, sp_tuple_max_dim> current;
@@ -430,17 +454,24 @@ public:
               total_send_count++;
               this->send_counts_cyclic[sending_procs[i]]++;
             }
-            SpTuple<VALUE_TYPE, sp_tuple_max_dim> latest = (*data_buffer_ptr)[sending_procs[i]][this->send_counts_cyclic[sending_procs[i]] -1];
+            SpTuple<VALUE_TYPE, sp_tuple_max_dim> latest =
+                (*data_buffer_ptr)[sending_procs[i]]
+                                  [this->send_counts_cyclic[sending_procs[i]] -
+                                   1];
             auto row_index_offset = latest.rows[0];
             auto col_index_offset = latest.rows[1];
-            if (row_index_offset >= row_max or col_index_offset >= sp_tuple_max_dim) {
+            if (row_index_offset >= row_max or
+                col_index_offset >= sp_tuple_max_dim) {
               SpTuple<VALUE_TYPE, sp_tuple_max_dim> current;
-              current.rows[0] = 2; // rows first two indices are already taken for metadata
+              current.rows[0] =
+                  2; // rows first two indices are already taken for metadata
               current.rows[1] = 0;
               (*data_buffer_ptr)[sending_procs[i]].push_back(current);
               total_send_count++;
               this->send_counts_cyclic[sending_procs[i]]++;
-              latest = (*data_buffer_ptr)[sending_procs[i]][this->send_counts_cyclic[sending_procs[i]] - 1];
+              latest = (*data_buffer_ptr)
+                  [sending_procs[i]]
+                  [this->send_counts_cyclic[sending_procs[i]] - 1];
               row_index_offset = latest.rows[0];
               col_index_offset = latest.rows[1];
             }
@@ -530,22 +561,44 @@ public:
     }
 
     add_datatransfers(total_receive_count, "Data transfers");
-//
+    //
     t = start_clock();
+
+    for (int i = 0; i < this->grid->col_world_size; i++) {
+      INDEX_TYPE base_index = this->sdispls_cyclic[i];
+      INDEX_TYPE count = this->send_counts_cyclic[i];
+
+      for (INDEX_TYPE j = base_index; j < base_index + count; j++) {
+        SpTuple<VALUE_TYPE, sp_tuple_max_dim> sp_tuple = (*sendbuf_cyclic)[j];
+        auto row_offset = sp_tuple.rows[0];
+        auto offset_so_far = 0;
+        for (auto k = 2; k < row_offset; k = k + 3) {
+          auto key = sp_tuple.rows[k];
+          auto data_count = sp_tuple.rows[k + 1];
+          auto tile = sp_tuple.rows[k + 2];
+
+          cout << " rank " << this->grid->rank_in_col << "batch id" << batch_id << "sending rank" << i << "key " << key << "data_count " << data_count << " tile " << tile << endl;
+
+        }
+      }
+    }
+
     MPI_Alltoallv((*sendbuf_cyclic).data(), this->send_counts_cyclic.data(),
                   this->sdispls_cyclic.data(), SPARSETUPLE,
                   (*receivebuf).data(), this->receive_counts_cyclic.data(),
                   this->rdispls_cyclic.data(), SPARSETUPLE,
                   this->grid->col_world);
-    this->store_remotely_computed_data(sendbuf_cyclic,receivebuf,iteration,batch_id);
-//    stop_clock_and_add(t, "Communication Time");
+    this->store_remotely_computed_data(sendbuf_cyclic, receivebuf, iteration,
+                                       batch_id);
+    //    stop_clock_and_add(t, "Communication Time");
   }
 
-  inline void store_remotely_computed_data(vector<SpTuple<VALUE_TYPE, sp_tuple_max_dim>> *sendbuf,
+  inline void store_remotely_computed_data(
+      vector<SpTuple<VALUE_TYPE, sp_tuple_max_dim>> *sendbuf,
       vector<SpTuple<VALUE_TYPE, sp_tuple_max_dim>> *receivebuf, int iteration,
       int batch_id) {
 
-//    #pragma omp parallel for
+    //    #pragma omp parallel for
     for (int i = 0; i < this->grid->col_world_size; i++) {
       INDEX_TYPE base_index = this->rdispls_cyclic[i];
       INDEX_TYPE count = this->receive_counts_cyclic[i];
@@ -558,24 +611,31 @@ public:
           auto key = sp_tuple.rows[k];
           auto data_count = sp_tuple.rows[k + 1];
           auto tile = sp_tuple.rows[k + 2];
-          key = key - (*receiver_proc_tile_map)[batch_id][i][tile].row_starting_index;
+          key = key -
+                (*receiver_proc_tile_map)[batch_id][i][tile].row_starting_index;
           if (data_count > 0) {
-            if ((*receiver_proc_tile_map)[batch_id][i][tile].mode==0){
-              cout<<" rank "<<this->grid->rank_in_col<<" wrong mode "<<"batch id"<<batch_id<<"incoming rank"<<i<<"key "<<key<<"data_count "<<data_count<<" tile "<<tile<<endl;
+            if ((*receiver_proc_tile_map)[batch_id][i][tile].mode == 0) {
+              cout << " rank " << this->grid->rank_in_col << " wrong mode "
+                   << "batch id" << batch_id << "incoming rank" << i << "key "
+                   << key << "data_count " << data_count << " tile " << tile
+                   << endl;
             }
-//            cout<<" rank "<<this->grid->rank_in_col<<" size "<<(*(*receiver_proc_tile_map)[batch_id][i][tile].dataCachePtr).size();
-//            SparseCacheEntry<VALUE_TYPE> cache_entry =(*(*receiver_proc_tile_map)[batch_id][i][tile].dataCachePtr)[key];
-//            auto entry_offset = cache_entry.cols.size();
-//            cache_entry.cols.resize(entry_offset + count);
-//            cache_entry.values.resize(entry_offset + count);
-//            copy(sp_tuple.cols.begin() + offset_so_far,
-//                 sp_tuple.cols.begin() + offset_so_far + count,
-//                 cache_entry.cols.begin() + entry_offset);
-//            copy(sp_tuple.values.begin() + offset_so_far,
-//                 sp_tuple.values.begin() + offset_so_far + count,
-//                 cache_entry.values.begin() + entry_offset);
-//            offset_so_far += count;
-//            (*(*receiver_proc_tile_map)[batch_id][i][tile].dataCachePtr)[key] = cache_entry;
+            //            cout<<" rank "<<this->grid->rank_in_col<<" size
+            //            "<<(*(*receiver_proc_tile_map)[batch_id][i][tile].dataCachePtr).size();
+            //            SparseCacheEntry<VALUE_TYPE> cache_entry
+            //            =(*(*receiver_proc_tile_map)[batch_id][i][tile].dataCachePtr)[key];
+            //            auto entry_offset = cache_entry.cols.size();
+            //            cache_entry.cols.resize(entry_offset + count);
+            //            cache_entry.values.resize(entry_offset + count);
+            //            copy(sp_tuple.cols.begin() + offset_so_far,
+            //                 sp_tuple.cols.begin() + offset_so_far + count,
+            //                 cache_entry.cols.begin() + entry_offset);
+            //            copy(sp_tuple.values.begin() + offset_so_far,
+            //                 sp_tuple.values.begin() + offset_so_far + count,
+            //                 cache_entry.values.begin() + entry_offset);
+            //            offset_so_far += count;
+            //            (*(*receiver_proc_tile_map)[batch_id][i][tile].dataCachePtr)[key]
+            //            = cache_entry;
           }
         }
       }
@@ -585,7 +645,6 @@ public:
     sendbuf->clear();
     sendbuf->shrink_to_fit();
   }
-
 };
 
 } // namespace distblas::net
