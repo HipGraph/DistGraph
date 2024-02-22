1980a5de1450538f210750b56bfcc8a99f15f3e7
Isuru Ranawaka <irjanith@gmail.com>
2024-02-22 11:14:38 -0500

    remote computations merge step

--- cpp/net/tile_based_data_comm.hpp
+++ cpp/net/tile_based_data_comm.hpp
@@ -595,7 +595,7 @@ public:
           auto tile = sp_tuple.rows[k + 2];
           key = key - (*receiver_proc_tile_map)[batch_id][i][tile].row_starting_index;
           if (((*receiver_proc_tile_map)[batch_id][i][tile].mode==0)){
-                cout<<" rank "<<grid->rank_in_col<<" batch_id  "<<batch_id<<" i "<<i<<" tile "<<tile<<endl;
+                cout<<" rank "<<this->grid->rank_in_col<<" batch_id  "<<batch_id<<" i "<<i<<" tile "<<tile<<endl;
            }
           SparseCacheEntry<VALUE_TYPE> cache_entry =(*(*receiver_proc_tile_map)[batch_id][i][tile].dataCachePtr)[key];
 //          auto entry_offset = cache_entry.cols.size();
