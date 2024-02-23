4c691a3483356aea295a27c59d3a0a8f705b5912
Isuru Ranawaka <irjanith@gmail.com>
2024-02-23 08:23:40 -0500

    remote computations merge step

--- cpp/algo/spgemm_with_tiling.hpp
+++ cpp/algo/spgemm_with_tiling.hpp
@@ -332,12 +332,10 @@ public:
                   while(count<max_count){
                     if ((*(output->sparse_data_collector))[index][hash].col==d){
                       (*(output->sparse_data_collector))[index][hash].value = (*(output->sparse_data_collector))[index][hash].value + value;
-                      cout<<(*(output->sparse_data_collector))[index][hash].value<<" ";
                       break;
                     }else if ((*(output->sparse_data_collector))[index][hash].col==-1){
                       (*(output->sparse_data_collector))[index][hash].col = d;
                       (*(output->sparse_data_collector))[index][hash].value =   value;
-                      cout<<(*(output->sparse_data_collector))[index][hash].value<<" ";
                       break;
                     }else {
                       hash = (hash+100) & (ht_size-1);
@@ -345,7 +343,6 @@ public:
                     }
                   }
                 }
-                cout<<endl;
               }else {
                 for (auto k = handle->rowStart[local_dst]; k < handle->rowStart[local_dst + 1]; k++) {
                   auto d = (handle->col_idx[k]);
@@ -368,10 +365,12 @@ public:
                   while (count<max_count) {
                     if ((*(output->sparse_data_collector))[index][hash].col == d) {
                       (*(output->sparse_data_collector))[index][hash].value = (*(output->sparse_data_collector))[index][hash].value + value;
+                      cout<<(*(output->sparse_data_collector))[index][hash].value<<" ";
                       break;
                     } else if ((*(output->sparse_data_collector))[index][hash].col ==-1) {
                       (*(output->sparse_data_collector))[index][hash].col = d;
                       (*(output->sparse_data_collector))[index][hash].value = value;
+                      cout<<(*(output->sparse_data_collector))[index][hash].value<<" ";
                       break;
                     } else {
                       hash =(hash + 100) &(ht_size -1);
@@ -388,6 +387,7 @@ public:
               }
             }
           }
+          cout<<endl;
         }
       }
     }
