5abd4ac38065489172ac0cb5ab8684b362bcf1fd
Isuru Ranawaka <irjanith@gmail.com>
2024-02-23 12:59:14 -0500

    remote computations merge step

--- cpp/dist_embed.cpp
+++ cpp/dist_embed.cpp
@@ -271,9 +271,9 @@ int main(int argc, char **argv) {
     cout << " rank " << rank << " spgemm algo started  " << endl;
     spgemm_algo.get()->algo_spgemm(iterations, batch_size,lr);
     cout << " rank " << rank << " spgemm algo completed  " << endl;
-//    output_sparsity = (sparse_out->csr_local_data)->handler->rowStart[(sparse_out->csr_local_data)->handler->rowStart.size()-1];
-//    output_sparsity = 100*(output_sparsity/(((sparse_out->csr_local_data)->handler->rowStart.size()-1)*dimension));
-//    reader->parallel_write_csr<double>(output_file+"/sparse_embedding.txt",(sparse_out->csr_local_data)->handler.get(),grid.get(), localARows,shared_sparseMat.get()->gRows,dimension);
+    output_sparsity = (sparse_out->csr_local_data)->handler->rowStart[(sparse_out->csr_local_data)->handler->rowStart.size()-1];
+    output_sparsity = 100*(output_sparsity/(((sparse_out->csr_local_data)->handler->rowStart.size()-1)*dimension));
+    reader->parallel_write_csr<double>(output_file+"/sparse_embedding.txt",(sparse_out->csr_local_data)->handler.get(),grid.get(), localARows,shared_sparseMat.get()->gRows,dimension);
 
   }else {
     auto dense_mat = shared_ptr<DenseMat<INDEX_TYPE, VALUE_TYPE, dimension>>(
