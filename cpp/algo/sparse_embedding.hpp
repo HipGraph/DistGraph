869f9f2395ee560d447debdcbc565edc151ca6ea
Isuru Ranawaka <irjanith@gmail.com>
2024-02-23 20:30:34 -0500

    implement sparse embedding

--- cpp/algo/sparse_embedding.hpp
+++ cpp/algo/sparse_embedding.hpp
@@ -196,9 +196,9 @@ public:
     int prev_end_process = get_end_proc(prev_start, beta, grid->col_world_size);
 
     // updating last remote fetched data vectors
-    this->calc_t_dist_grad_rowptr(csr_block,  lr, iteration,batch,
-                                  batch_size, considering_batch_size,
-                                  1,prev_start, prev_end_process,symbolic, main_comm, output);
+//    this->calc_t_dist_grad_rowptr(csr_block,  lr, iteration,batch,
+//                                  batch_size, considering_batch_size,
+//                                  1,prev_start, prev_end_process,symbolic, main_comm, output);
     // dense_local->invalidate_cache(i, j, true);
   }
 
