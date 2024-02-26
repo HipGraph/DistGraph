bbd7f985551810dc131d4a13047bfbd4f3cc1d55
Isuru Ranawaka <irjanith@gmail.com>
2024-02-25 21:48:23 -0500

    remote output collector

--- cpp/algo/sparse_embedding.hpp
+++ cpp/algo/sparse_embedding.hpp
@@ -396,36 +396,36 @@ public:
                 while(count<total_count){
                   auto  local_d =  (local_tracker<local_tracker_end)?local_handle->col_idx[local_tracker]:INT_MAX;
                   auto remote_d =  (remote_tracker<remote_tracker_end)?handle->col_idx[remote_tracker]:INT_MAX;
-                  if (local_d==INT_MAX and remote_d==INT_MAX){
-                    break;
-                  }else if (remote_d==INT_MAX or local_d<remote_d){
-                    auto local_value = local_handle->values[local_tracker];
-                    VALUE_TYPE attrc = local_value * local_value;
-                    VALUE_TYPE d1 = -2.0 / (1.0 + attrc);
-                    VALUE_TYPE l = scale( attrc* d1);
-                    (*(output->dense_collector))[index][local_d] +=  (lr)*l;
-                    local_tracker++;
-                    count++;
-                  }else if (local_d==INT_MAX or remote_d<local_d){
-                    auto remote_value = handle->values[remote_tracker];
-                    VALUE_TYPE attrc = remote_value * remote_value;
-                    VALUE_TYPE d1 = -2.0 / (1.0 + attrc);
-                    VALUE_TYPE l = scale( attrc* d1);
-                    (*(output->dense_collector))[index][remote_d] +=  (lr)*l;
-                    remote_tracker++;
-                    count++;
-                  }else{
-                    auto local_value = local_handle->values[local_tracker];
-                    auto remote_value = handle->values[remote_tracker];
-                    VALUE_TYPE value = local_value - remote_value;
-                    VALUE_TYPE attrc = value * value;
-                    VALUE_TYPE d1 = -2.0 / (1.0 + attrc);
-                    VALUE_TYPE l = scale( attrc* d1);
-                    (*(output->dense_collector))[index][remote_d] +=  (lr)*l;
-                    local_tracker++;
-                    remote_tracker++;
-                    count = count+2;
-                  }
+//                  if (local_d==INT_MAX and remote_d==INT_MAX){
+//                    break;
+//                  }else if (remote_d==INT_MAX or local_d<remote_d){
+//                    auto local_value = local_handle->values[local_tracker];
+//                    VALUE_TYPE attrc = local_value * local_value;
+//                    VALUE_TYPE d1 = -2.0 / (1.0 + attrc);
+//                    VALUE_TYPE l = scale( attrc* d1);
+//                    (*(output->dense_collector))[index][local_d] +=  (lr)*l;
+//                    local_tracker++;
+//                    count++;
+//                  }else if (local_d==INT_MAX or remote_d<local_d){
+//                    auto remote_value = handle->values[remote_tracker];
+//                    VALUE_TYPE attrc = remote_value * remote_value;
+//                    VALUE_TYPE d1 = -2.0 / (1.0 + attrc);
+//                    VALUE_TYPE l = scale( attrc* d1);
+//                    (*(output->dense_collector))[index][remote_d] +=  (lr)*l;
+//                    remote_tracker++;
+//                    count++;
+//                  }else{
+//                    auto local_value = local_handle->values[local_tracker];
+//                    auto remote_value = handle->values[remote_tracker];
+//                    VALUE_TYPE value = local_value - remote_value;
+//                    VALUE_TYPE attrc = value * value;
+//                    VALUE_TYPE d1 = -2.0 / (1.0 + attrc);
+//                    VALUE_TYPE l = scale( attrc* d1);
+//                    (*(output->dense_collector))[index][remote_d] +=  (lr)*l;
+//                    local_tracker++;
+//                    remote_tracker++;
+//                    count = count+2;
+//                  }
                 }
               }
             } else {
@@ -553,7 +553,7 @@ public:
   }
 
   void merge_output_to_input(int batch_id) {
-//    this->sparse_local->initialize_CSR_from_dense_collector( this->sparse_local->proc_row_width,embedding_dim,false);
+    this->sparse_local->initialize_CSR_from_dense_collector( this->sparse_local->proc_row_width,embedding_dim,false);
   }
 
   VALUE_TYPE scale(VALUE_TYPE v) {
