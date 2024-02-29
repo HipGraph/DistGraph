f96b7cc2af8a76bc59c29823268c16200f34e93d
Isuru Ranawaka <irjanith@gmail.com>
2024-02-28 20:41:10 -0500

    bfs update

--- cpp/algo/multi_source_bfs.hpp
+++ cpp/algo/multi_source_bfs.hpp
@@ -63,21 +63,34 @@ public:
       auto rows =  sp_local_receiver->proc_row_width;
       auto cols = static_cast<INDEX_TYPE>(embedding_dim);
       auto sparse_out = make_shared<distblas::core::SpMat<VALUE_TYPE>>(grid,rows,cols,hash_spgemm);
-      unique_ptr<distblas::algo::SpGEMMAlgoWithTiling<INDEX_TYPE, VALUE_TYPE,
-                                                      embedding_dim>>
+      unique_ptr<distblas::algo::SpGEMMAlgoWithTiling<INDEX_TYPE, VALUE_TYPE,embedding_dim>>
           spgemm_algo = unique_ptr<distblas::algo::SpGEMMAlgoWithTiling<
               INDEX_TYPE, VALUE_TYPE, embedding_dim>>(
               new distblas::algo::SpGEMMAlgoWithTiling<INDEX_TYPE, VALUE_TYPE,
                                                        embedding_dim>(
                   sp_local_native, sp_local_receiver, sp_local_sender,
                   sparse_input, sparse_out.get(), grid, alpha, beta, col_major,
-                  sync, tile_width_fraction, hash_spgemm));
+                  sync, tile_width_fraction, hash_spgemm,state_holder.get()));
+
       spgemm_algo.get()->algo_spgemm(1, batch_size, lr);
+      this->update_state_holder(sparse_input,state_holder.get());
 
       output_sparsity =(sparse_out->csr_local_data)->handler->rowStart[(sparse_out->csr_local_data)->handler->rowStart.size() - 1];
       output_sparsity =100 * (output_sparsity /(((sparse_out->csr_local_data)->handler->rowStart.size() - 1) *embedding_dim));
       cout << " rank " << grid->rank_in_col << " output sparsity "<< output_sparsity << endl;
       (*(sparse_input->csr_local_data)) = (*(sparse_out->csr_local_data));
+    }
+  }
+
+  void update_state_holder( distblas::core::SpMat<VALUE_TYPE> *sparse_input, DenseMat<INDEX_TYPE,VALUE_TYPE> * dense_mat){
+    CSRHandle *handle = sparse_input->csr_local_data->handler.get();
+    #pragma omp paralle for
+    for(auto i=0;i<handle->rowStart.size()-1;i++){
+      for(auto j=handle->rowStart[i];j<handle->rowStart[i+1];j++){
+        auto d = handle->col_idx[j];
+        (*(dense_mat->state_metadata))[i][d]=1;
+        (*(dense_mat->nnz_count))[i]++;
+      }
     }
   }
 };
