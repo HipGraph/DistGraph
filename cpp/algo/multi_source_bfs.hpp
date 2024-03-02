309eb0e503035463ddcfcc3e60f17dfc4545fb3b
Isuru Ranawaka <irjanith@gmail.com>
2024-03-02 12:55:14 -0500

    bfs update

--- cpp/algo/multi_source_bfs.hpp
+++ cpp/algo/multi_source_bfs.hpp
@@ -68,7 +68,7 @@ public:
 
       auto density =   (bfs_frontier/(sp_local_receiver->proc_row_width*embedding_dim))*100;
       bool enable_remote = density>1.0?true:false;
-
+      cour<<" iteration "<<i<<" enable remote "<<enable_remote<<endl;
       unique_ptr<distblas::algo::SpGEMMAlgoWithTiling<INDEX_TYPE, VALUE_TYPE,embedding_dim>>
           spgemm_algo = unique_ptr<distblas::algo::SpGEMMAlgoWithTiling<
               INDEX_TYPE, VALUE_TYPE, embedding_dim>>(
