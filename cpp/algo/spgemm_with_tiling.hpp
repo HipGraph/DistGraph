b5ad1b7ca4a6a90f389145fbce63563a32c3574d
Isuru Ranawaka <irjanith@gmail.com>
2024-03-02 12:06:24 -0500

    bfs update

--- cpp/algo/spgemm_with_tiling.hpp
+++ cpp/algo/spgemm_with_tiling.hpp
@@ -149,10 +149,10 @@ public:
               csr_block, batch_size, considering_batch_size, lr, 1, 0, true,
               false, this->sparse_local_output);
           if (enable_remote) {
-//            this->calc_t_dist_grad_rowptr(
-//                (this->sp_local_sender)->csr_local_data.get(), lr, i, j,
-//                batch_size, considering_batch_size, 2, 0,
-//                this->grid->col_world_size, false, main_comm.get(), nullptr);
+            this->calc_t_dist_grad_rowptr(
+                (this->sp_local_sender)->csr_local_data.get(), lr, i, j,
+                batch_size, considering_batch_size, 2, 0,
+                this->grid->col_world_size, false, main_comm.get(), nullptr);
 //            main_comm->receive_remotely_computed_data(
 //                sendbuf_ptr.get(), update_ptr.get(), i, j, 0,
 //                this->grid->col_world_size, 0, total_tiles);
