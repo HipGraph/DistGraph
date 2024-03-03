ef539be6f8d8250a4ceeb40e5b771f53ba21ae6d
Isuru Ranawaka <irjanith@gmail.com>
2024-03-02 20:21:51 -0500

    bfs update

--- cpp/algo/spgemm_with_tiling.hpp
+++ cpp/algo/spgemm_with_tiling.hpp
@@ -336,6 +336,9 @@ public:
             if (fetch_from_cache) {
               unordered_map<INDEX_TYPE, SparseCacheEntry<VALUE_TYPE>>
                   &arrayMap = (*this->sparse_local->tempCachePtr)[target_rank];
+              if (arrayMap.find(dst_id)==arrayMap.end()){
+                continue; //this key does not contain any data.
+              }
               remote_cols = arrayMap[dst_id].cols;
               remote_values = arrayMap[dst_id].values;
             }
