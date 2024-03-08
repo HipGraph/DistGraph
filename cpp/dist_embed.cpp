6ae502c58153a61fb55febae081958e7313e249a
Isuru Ranawaka <irjanith@gmail.com>
2024-03-08 09:09:37 -0500

    bfs update

--- cpp/dist_embed.cpp
+++ cpp/dist_embed.cpp
@@ -302,7 +302,7 @@ int main(int argc, char **argv) {
 
     MPI_Barrier(MPI_COMM_WORLD);
     cout << " rank " << rank << " spgemm algo started  " << endl;
-    perf_stats =  spgemm_algo.get()->execute(iterations, batch_size,lr);
+//    perf_stats =  spgemm_algo.get()->execute(iterations, batch_size,lr);
     cout << " rank " << rank << " spgemm algo completed  " << endl;
 //    output_sparsity = (sparse_out->csr_local_data)->handler->rowStart[(sparse_out->csr_local_data)->handler->rowStart.size()-1];
 //    output_sparsity = 100*(output_sparsity/(((sparse_out->csr_local_data)->handler->rowStart.size()-1)*dimension));
