5fdb1072e5587c4b0478f8c104391e189a77fdb8
Isuru Ranawaka <irjanith@gmail.com>
2024-03-09 17:31:18 -0500

    bfs update

--- cpp/algo/multi_source_bfs.hpp
+++ cpp/algo/multi_source_bfs.hpp
@@ -102,7 +102,6 @@ public:
       spgemm_algo.get()->algo_spgemm(1, batch_size, lr,false);
       stop_clock_and_add(t, "Total Time");
       double totalLocalSpGEMM = std::accumulate((spgemm_algo->timing_info).begin(), (spgemm_algo->timing_info).end(), 0.0);
-      cout<<"SpGEMM"<<totalLocalSpGEMM<<endl;
       add_perf_stats(totalLocalSpGEMM,"Local SpGEMM");
 //      t = start_clock();
 //      sparse_out->initialize_CSR_blocks(false);
