fb9d948de4c11d0df8b31c75f8ec71bb21c29387
Isuru Ranawaka <irjanith@gmail.com>
2024-03-22 17:24:23 -0400

    bfs update

--- cpp/net/data_comm.hpp
+++ cpp/net/data_comm.hpp
@@ -534,8 +534,8 @@ public:
           auto key = sp_tuple.rows[k];
           auto copying_count = sp_tuple.rows[k + 1];
           if (force_delete or (*(this->sparse_local)->tempCachePtr)[i].find(key) ==
-              (*(this->sparse_local)->tempCachePtr)[i].end() or ((*(this->sparse_local)->tempCachePtr)[i][key].inserted_itr==iteration
-               or (*(this->sparse_local)->tempCachePtr)[i][key].inserted_batch_id)==batch_id or ((*(this->sparse_local)->tempCachePtr)[i][key].force_delete)) {
+              (*(this->sparse_local)->tempCachePtr)[i].end() or ((*(this->sparse_local)->tempCachePtr)[i][key].inserted_itr!=iteration
+               or (*(this->sparse_local)->tempCachePtr)[i][key].inserted_batch_id)!=batch_id or ((*(this->sparse_local)->tempCachePtr)[i][key].force_delete)) {
             SparseCacheEntry<VALUE_TYPE> sp_entry;
             sp_entry.inserted_itr = iteration;
             sp_entry.inserted_batch_id = batch_id;
