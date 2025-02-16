dfb6cc47270c98e15d2779cd73fee8d1f0e95687
isururanawaka <irjanith@gmail.com>
2025-02-16 11:49:38 -0500

    adding computeGAT method

--- cpp/dist_embed.cpp
+++ cpp/dist_embed.cpp
@@ -317,8 +317,8 @@ int main(int argc, char **argv) {
                       shared_sparseMat_sender.get(),grid.get(),
                       alpha, beta,col_major,sync_comm, tile_width_fraction,false);
       gat->addLayer(distblas::algo::GATLayer<INDEX_TYPE,VALUE_TYPE,256>(256,4));
-      gat->addLayer(distblas::algo::GATLayer<INDEX_TYPE,VALUE_TYPE,256>(1024,4));
-      gat->addLayer(distblas::algo::GATLayer<INDEX_TYPE,VALUE_TYPE,256>(1024,6));
+//      gat->addLayer(distblas::algo::GATLayer<INDEX_TYPE,VALUE_TYPE,256>(1024,4));
+//      gat->addLayer(distblas::algo::GATLayer<INDEX_TYPE,VALUE_TYPE,256>(1024,6));
 
       MPI_Barrier(MPI_COMM_WORLD);
       cout << " rank " << rank << " gat algo started  " << endl;
