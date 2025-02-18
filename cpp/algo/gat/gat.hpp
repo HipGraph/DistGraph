60d31f07bdc03a22b8c9a7baf61c0f0e273e0d09
isururanawaka <irjanith@gmail.com>
2025-02-18 18:38:49 -0500

    adding data communication

--- cpp/algo/gat/gat.hpp
+++ cpp/algo/gat/gat.hpp
@@ -60,7 +60,7 @@ namespace distblas::algo {
 
         void computeGAT(int i, int j){
             auto  dense_output = make_unique<DenseMat<INDEX_TYPE,VALUE_TYPE>>(grid,buffers[i]->rows,gat_layers[i].weights[j]->cols,true);
-            cout<<" dense computing multiplications  "<<i<<"  head "<<j<<" started "<<endl;
+            cout<<" dense computing multiplications  "<<i<<"  head "<<j<<" started size "<<buffers[i]->rows*gat_layers[i].weights[j]->cols<<endl;
             buffers[i]->multiply(gat_layers[i].weights[j].get(),dense_output.get());
 
             cout<<" dense computing layer  "<<i<<"  head "<<j<<" completed "<<endl;
