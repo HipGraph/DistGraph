67e974db3384c4f9b7c5e0a83867598216940e7b
isururanawaka <irjanith@gmail.com>
2025-02-22 21:34:26 -0500

    fix deep copying issue in SpMat

--- cpp/algo/gat/gat.hpp
+++ cpp/algo/gat/gat.hpp
@@ -65,8 +65,8 @@ namespace distblas::algo {
                 for (int k = start_index; k < start_index+gat_layers[i].features_per_head; k++) {
                     VALUE_TYPE val = -1.0 + 2.0 * rand() / (RAND_MAX + 1.0);
 //                    buffers[i]->nCoordinates[i * buffers[i]->cols + k]=0.0;
-                    VALUE_TYPE  val = input->nCoordinates[i * input->cols + k-start_index];
-                    if (val==0){
+                    VALUE_TYPE  value = input->nCoordinates[i * input->cols + k-start_index];
+                    if (value==0){
                         count++;
                     }
                 }
