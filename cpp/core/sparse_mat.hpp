d07e9a799ed2d14e84490f70bcba9c5cb7b44fa3
isururanawaka <irjanith@gmail.com>
2025-03-31 13:13:53 -0400

    handler creation completed

--- cpp/core/sparse_mat.hpp
+++ cpp/core/sparse_mat.hpp
@@ -208,6 +208,7 @@ namespace distblas::core {
                     auto end_index = std::min(
                             static_cast<INDEX_TYPE>((procs[r] + 1) * proc_col_width), gCols);
                     for (int i = starting_index; i < end_index; i++) {
+                        cout << " rank " << grid->rank_in_col << "  working on rank "<<i << endl;
                         if (rank != procs[r] and
                             (handle->rowStart[i + 1] - handle->rowStart[i]) > 0) {
                             for (auto j = handle->rowStart[i]; j < handle->rowStart[i + 1];
