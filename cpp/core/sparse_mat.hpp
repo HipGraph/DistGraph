c1c1cc796ddc98adc9b555f10eed278fffb7ac14
isururanawaka <irjanith@gmail.com>
2025-03-31 13:20:10 -0400

    handler creation completed

--- cpp/core/sparse_mat.hpp
+++ cpp/core/sparse_mat.hpp
@@ -224,6 +224,7 @@ namespace distblas::core {
                             }
                         }
                     }
+                    cout << " rank " << grid->rank_in_col << "  working on rank "<<procs[r] << " completed " endl;
                 }
             }
         }
