6ceb7cf95d5fbca4b45adcad6e0e9b15648c251a
isururanawaka <irjanith@gmail.com>
2025-03-31 09:52:39 -0400

    spmm passed

--- cpp/algo/spmm/spmm.hpp
+++ cpp/algo/spmm/spmm.hpp
@@ -34,7 +34,7 @@ public:
       this->input_dense_mat=input_dense_mat;
       this->output = output;
       cout << " rank " << this->grid->rank_in_col << " SpMMAlgo constructor calling" << endl;
-      this->sp_local_receiver->csr_local_data.get())->handler.get();
+      this->sp_local_receiver->csr_local_data.get()->handler.get();
       cout << " rank "<< this->grid->rank_in_col  << "access handler passed "<< endl;
       auto embedding_algo =
               make_unique<distblas::algo::SpMMAlgo<INDEX_TYPE, VALUE_TYPE>>(
