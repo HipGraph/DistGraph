f11c4a08463cdfdc9ee7847cfb695ad18a70790f
isururanawaka <irjanith@gmail.com>
2025-03-31 09:22:23 -0400

    adding debug logs

--- cpp/net/data_comm.hpp
+++ cpp/net/data_comm.hpp
@@ -111,10 +111,12 @@ public:
       this->sp_local_receiver->find_col_ids(batch_id, 0, grid->col_world_size,
                                             receive_col_ids_list,
                                             receive_indices_to_proc_map, 0);
+        cout << " rank " << grid->rank_in_col << "  sp_local_receiver find_col_ids completed "  << endl;
       // calculating sending data cols
       this->sp_local_sender->find_col_ids(batch_id, 0, grid->col_world_size,
                                           send_col_ids_list,
                                           send_indices_to_proc_map, 0);
+        cout << " rank " << grid->rank_in_col << "  sp_local_sender find_col_ids completed "  << endl;
     } else if (alpha == 1.0) {
       // This represents the case for pushing
       this->sp_local_receiver->find_col_ids(batch_id, 0, grid->col_world_size,
