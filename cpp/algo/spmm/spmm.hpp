2ccba0f8aa8808ab3bce334ff3a86d8083542efe
isururanawaka <irjanith@gmail.com>
2025-03-31 08:29:14 -0400

    adding spmm

--- cpp/algo/spmm/spmm.hpp
+++ cpp/algo/spmm/spmm.hpp
@@ -38,8 +38,9 @@ public:
                       this->sparse_local, this->sp_local_receiver,
                       this->sp_local_sender,input_dense_mat,
                       output, this->grid, this->alpha, this->beta, false);
-      spMMAlgo = embedding_algo.get();
       cout << " rank " << this->grid->rank_in_col << " spmm algo initialization completed" << endl;
+      spMMAlgo = embedding_algo.get();
+      cout << " rank " << this->grid->rank_in_col << " spmm algo assign completed" << endl;
   }
 
   json execute() {
