3988891b94e389cac6120a1b27f8f20f27e5eae5
isururanawaka <irjanith@gmail.com>
2025-04-15 12:07:38 -0400

    debugging inheritance issue

--- cpp/algo/spmm/spmm.hpp
+++ cpp/algo/spmm/spmm.hpp
@@ -29,54 +29,42 @@ public:
        DenseMat<INDEX_TYPE,VALUE_TYPE>* output,
        double alpha, double beta){
 
-      this->sparse_local = sparse_mat;
-      this->grid=grid;
-      this->alpha=alpha;
-      this->beta=beta;
       auto localBRows = divide_and_round_up(sparse_mat->gCols,grid->col_world_size);
       auto localARows = divide_and_round_up(sparse_mat->gRows,grid->col_world_size);
-      this->batch_size = localARows;
 
-      this->sparse_local->batch_size = this->batch_size;
-      this->sparse_local->proc_row_width = localARows;
-      this->sparse_local->proc_col_width = localBRows;
+      sparse_mat->batch_size = localARows;
+      sparse_mat->proc_row_width = localARows;
+      sparse_mat->proc_col_width = localBRows;
 
       vector<Tuple<VALUE_TYPE>> copiedVector(sparse_mat->coords);
       auto shared_sparseMat_sender = make_shared<distblas::core::SpMat<INDEX_TYPE,VALUE_TYPE>>(grid,
                                                                                                copiedVector, sparse_mat->gRows,
-                                                                                               sparse_mat->gCols, sparse_mat->gNNz, this->batch_size,
+                                                                                               sparse_mat->gCols, sparse_mat->gNNz, sparse_mat->batch_size,
                                                                                                localARows, localBRows, false, true);
-      this->sp_local_sender =  shared_sparseMat_sender.get();
 
 
       auto shared_sparseMat_receiver = make_shared<distblas::core::SpMat<INDEX_TYPE,VALUE_TYPE>>(grid,
                                                                                                  copiedVector, sparse_mat->gRows,
-                                                                                                 sparse_mat->gCols, sparse_mat->gNNz, this->batch_size,
+                                                                                                 sparse_mat->gCols, sparse_mat->gNNz, sparse_mat->batch_size,
                                                                                                  localARows, localBRows, true, false);
-
-      this->sp_local_receiver = shared_sparseMat_receiver.get();
 
       auto partitioner = unique_ptr<GlobalAdjacency1DPartitioner>(new GlobalAdjacency1DPartitioner(grid));
 
-      partitioner.get()->partition_data<INDEX_TYPE,VALUE_TYPE>(this->sp_local_sender);
-      partitioner.get()->partition_data<INDEX_TYPE,VALUE_TYPE>(this->sp_local_receiver);
-      partitioner.get()->partition_data<INDEX_TYPE,VALUE_TYPE>(this->sparse_local);
+      partitioner.get()->partition_data<INDEX_TYPE,VALUE_TYPE>(shared_sparseMat_sender.get());
+      partitioner.get()->partition_data<INDEX_TYPE,VALUE_TYPE>(shared_sparseMat_receiver.get());
+      partitioner.get()->partition_data<INDEX_TYPE,VALUE_TYPE>(sparse_mat);
 
-      this->sparse_local->initialize_CSR_blocks(true);
-      this->sp_local_sender->initialize_CSR_blocks(true);
-      this->sp_local_receiver->initialize_CSR_blocks(true);
-
-
-      this->input_dense_mat=input_dense_mat;
-      this->output = output;
+      shared_sparseMat_sender->initialize_CSR_blocks(true);
+      shared_sparseMat_receiver->initialize_CSR_blocks(true);
+      sparse_mat->initialize_CSR_blocks(true);
 
       auto embedding_algo =
               make_unique<distblas::algo::SpMMAlgo<INDEX_TYPE, VALUE_TYPE>>(
-                      this->sparse_local, this->sp_local_receiver,
-                      this->sp_local_sender,input_dense_mat,
-                      output, this->grid, this->alpha, this->beta, false);
+                      sparse_mat.get(), shared_sparseMat_receiver.get(),
+                      shared_sparseMat_sender.get(),input_dense_mat,
+                      output, grid, alpha, beta, false);
       spMMAlgo = embedding_algo.get();
-      spMMAlgo->execute(1, this->batch_size, 1.0);
+      spMMAlgo->execute(1, sparse_mat->batch_size, 1.0);
   }
 };
 }
